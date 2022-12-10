4cff0b98f43dba4ea9eaa3274dcc87c53afc1dd2
Yvanoff <petruzzi.yvan@gmail.com>
2022-12-10 15:38:35 +0100

    Router on, changed logo, resetted basic css styles

---
+++ frontend/src/views/IndexView.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>INDEX</h1>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<template>
+<h1>Salut</h1>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
