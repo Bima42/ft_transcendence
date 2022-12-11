515260f02152306b46ed65bb4955887e8a37aba3
Yvanoff <petruzzi.yvan@gmail.com>
2022-12-11 22:41:33 +0100

    modified path to env. file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,7 +8,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: [`stage.${process.env.STAGE}.env`],
+      envFilePath: [`../.env`],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
