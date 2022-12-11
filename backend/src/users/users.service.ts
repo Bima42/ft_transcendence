95880efdbd33ed201ba3bdcc65a46525f7cd8b76
Bima42 <tanpau42@gmail.com>
2022-12-11 20:25:24 +0100

    Adding database container and pgadmin. At this point, everything work properly but db config is static, i want to change that

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  getAllUsers(): Promise<Array<User>> {
+    return this.usersRepository.find({});
+  }
+}
