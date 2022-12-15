1fe6dd4c235cfe40c6891a6befe010f4cd2dfb55
Yvanoff <petruzzi.yvan@gmail.com>
2022-12-15 19:38:08 +0100

    main menu created and added some style here and there

--- frontend/src/views/CommunityView.vue
+++ frontend/src/views/CommunityView.vue
@@ -1,7 +1,11 @@
 <template>
+<header-pack></header-pack>
 </template>
 
 <script setup lang="ts">
+import HeaderPack from "@/components/headers/HeaderPack.vue";
+
+const props = defineProps<{}>()
 </script>
 
 <style scoped lang="scss">
