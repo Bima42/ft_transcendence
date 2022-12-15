02f1791ac6e40e418022946acd2e584c9773a9b3
Yvanoff <petruzzi.yvan@gmail.com>
2022-12-15 22:20:04 +0100

    small refactor

--- frontend/src/views/ScoreBoardView.vue
+++ frontend/src/views/ScoreBoardView.vue
@@ -1,11 +1,13 @@
 <template>
   <header-pack></header-pack>
   <MainMenu position="left"></MainMenu>
+  <CreditLink></CreditLink>
 </template>
 
 <script setup lang="ts">
 import HeaderPack from "@/components/headers/HeaderPack.vue";
 import MainMenu from "@/components/MainMenu.vue";
+import CreditLink from "@/components/footers/CreditLink.vue";
 
 const props = defineProps<{}>()
 </script>
