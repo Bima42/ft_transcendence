9be9acd63411adddf2cebe98e101d3bcd6bd6678
Yvanoff <petruzzi.yvan@gmail.com>
2022-12-15 19:48:08 +0100

    added the left property to the main menu

--- frontend/src/views/LiveGameView.vue
+++ frontend/src/views/LiveGameView.vue
@@ -1,9 +1,11 @@
 <template>
-<header-pack></header-pack>
+  <header-pack></header-pack>
+  <MainMenu position="left"></MainMenu>
 </template>
 
 <script setup lang="ts">
 import HeaderPack from "@/components/headers/HeaderPack.vue";
+import MainMenu from "@/components/MainMenu.vue";
 
 const props = defineProps<{}>()
 </script>
