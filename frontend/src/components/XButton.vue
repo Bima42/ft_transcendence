2cf0b94dac38de855b6c43357f86da1ae76aeef1
Yvanoff <petruzzi.yvan@gmail.com>
2022-12-16 01:21:51 +0100

    not working but still implementing it

---
+++ frontend/src/components/XButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button>X</button>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+button {
+  width: 60px;
+  height: 60px;
+  border: none;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  background-color: #fff;
+  border-radius: 50%;
+  margin-left: 83.4px;
+  font-size: 28pt;
+  &:hover {
+    cursor: pointer;
+  }
+}
+</style>
---
+++ frontend/src/stores/modal.ts
@@ -0,0 +1,40 @@
+import {defineStore} from 'pinia'
+import {ref, shallowRef} from "vue"
+import type {Component} from "vue";
+import type {Ref} from "vue"
+
+
+export const useModalStore = defineStore('modal', () => {
+    const show = ref(false)
+    const type: Ref<{}> & { "[ShallowRefMarker]"?: true } = shallowRef({})
+    const component: Ref<{}> & { "[ShallowRefMarker]"?: true } = shallowRef({})
+    const data = ref({})
+
+    const resetState = function () {
+        show.value = false
+        type.value = {}
+        component.value = {}
+        data.value = {}
+    }
+
+    const loadAndDisplay = function (newType: Component, newComponent: Component, newData: any) {
+        type.value = newType;
+        component.value = newComponent;
+        data.value = newData;
+        show.value = true;
+    }
+
+    const toggleModal = function () {
+        show.value = !show.value
+    }
+
+    return {
+        show,
+        type,
+        component,
+        data,
+        resetState,
+        loadAndDisplay,
+        toggleModal,
+    }
+})
