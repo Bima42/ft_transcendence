5e4bb9b56dd430c2e2b7b0b2f3705cccf03cbfbe
Bima42 <tanpau42@gmail.com>
2022-12-16 18:38:25 +0100

    Following tutorial, simple authentication is added using JWT strategy. Nothing is dynamic

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
+// import { User } from './user.entity';
+
+export type User = any;
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectRepository(User) private usersRepository: Repository<User>,
-  ) {}
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
 
-  getAllUsers(): Promise<Array<User>> {
-    return this.usersRepository.find({});
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
   }
 }
