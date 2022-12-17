c4053658ac38679c93c0b32ab4f23842a01028fa
santonie <sandrine.antonietti@gmail.com>
2022-12-17 16:51:36 +0100

    WIP CustomTable

---
+++ frontend/src/views/TableView.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="tableDiv">
+
+		<CustomTable></CustomTable>
+
+	</div>
+</template>
+
+<script setup lang="ts">
+	import CustomTable from '@/components/CustomTable.vue'
+
+	import { ref } from 'vue'
+	import { useRouter } from 'vue-router'
+
+	const router = useRouter()
+
+	function loginClick(e: Event) {
+	  router.push('/table')
+	}
+</script>
+
+<style scoped lang="scss">
+.tableDiv {
+	position:absolute;
+	top:50%;
+	left:50%;
+}
+</style>
