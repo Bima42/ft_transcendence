3e12f6fb2be1922a740ab04fb8b9dae2e93428b3
Bima42 <tanpau42@gmail.com>
2022-12-19 18:02:06 +0100

    Adding database module to set db correctly. Re-add users.controller to see datas stored in db. Guard now check inside the db to find a user

---
+++ backend/src/modules/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from '../users/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('POSTGRES_HOST'),
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DB'),
+          autoLoadEntities: true,
+          synchronize: true, //false for production -> lose data
+          entities: [User],
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
--- backend/src/users/user.entity.ts
+++ backend/src/users/user.entity.ts
@@ -1,10 +1,13 @@
-import { Entity } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   userId: number;
 
+  @Column()
   username: string;
 
+  @Column()
   password: string;
 }
