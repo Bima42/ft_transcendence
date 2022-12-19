7051f36e3f7a066984255dd0cb580e48eaffc751
Bima42 <tanpau42@gmail.com>
2022-12-19 18:02:06 +0100

    Adding database module to set db correctly. Re-add users.controller to see datas stored in db. Guard now check inside the db to find a user

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,24 +1,22 @@
 import { Injectable } from '@nestjs/common';
-// import { User } from './user.entity';
-
-export type User = any;
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.username === username);
+  async findOne(username: string): Promise<User> {
+    return this.usersRepository.findOne({
+      where: {
+        username: username,
+      },
+    });
+  }
+  getAllUsers(): Promise<Array<User>> {
+    return this.usersRepository.find({});
   }
 }
