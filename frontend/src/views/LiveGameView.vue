696ed0ff6ddd89c95cbe8fdc50c51694d10bed15
Yvanoff <petruzzi.yvan@gmail.com>
2022-12-20 15:28:27 +0100

    from div to table, css turned to scss and reviewed component logic

--- frontend/src/views/LiveGameView.vue
+++ frontend/src/views/LiveGameView.vue
@@ -1,30 +1,11 @@
 <template>
-	<div class="tableDiv">
-
-		<CustomTable></CustomTable>
-
-	</div>
+  <TheTable></TheTable>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{}>()
-	import CustomTable from '@/components/CustomTable.vue'
-
-	import { ref } from 'vue'
-	import { useRouter } from 'vue-router'
-
-	const router = useRouter()
-
-	function loginClick(e: Event) {
-	  router.push('/live')
-	}
+import TheTable from "@/components/table/TheTable.vue";
 </script>
 
 <style scoped lang="scss">
-.tableDiv {
-  position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-}
+
 </style>
