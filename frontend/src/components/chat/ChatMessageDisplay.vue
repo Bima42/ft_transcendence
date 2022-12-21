4f4fb5ea4b67b67b791711f7ccfa87e716db2260
santonie <sandrine.antonietti@gmail.com>
2022-12-21 15:32:14 +0100

    Move chat into components (finish)

---
+++ frontend/src/components/chat/ChatMessageDisplay.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="chat-messages">
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>testeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<h5>test</h5>
+		<!--Chat messages will be added here dynamically using JavaScript; example-->
+	</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+	#chat .chat-messages {
+		overflow-y: auto;
+		overflow-x: auto;
+		height: 407px;
+		width: 100%;
+		padding: 10px;
+		margin-bottom: 10px;
+	}
+	#chat .chat-message .chat-message-text {
+		margin-left: 10px;
+	}
+	#chat .chat-message .chat-message-sender {
+		font-weight: bold;
+	}
+</style>
