f26c638b21b0414b696449dca75e319bc6367cf3
santonie <sandrine.antonietti@gmail.com>
2022-12-21 15:32:14 +0100

    Move chat into components (finish)

---
+++ frontend/src/components/chat/ChatHeader.vue
@@ -0,0 +1,59 @@
+<template>
+	<div class="chat-header">
+				<h1>Chat</h1>
+			<div class="chat-tabs">
+				<button class="chat-tab" id="tab1">General Chat Room</button>
+				<button class="chat-tab" id="tab2">Private Chat Room</button>
+			</div>
+		</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+  #chat .chat-header {
+	background-color: #403c3e5d;
+	border-bottom: 1px solid rgb(255, 200, 0);
+	padding: 10px;
+	text-align: center;
+  }
+
+  #chat .chat-header h1 {
+	margin: 0;
+	color: rgb(255, 208, 0);
+	font-size: 1.2em;
+	font-weight: bolder;
+	padding-bottom: 20px;
+  }
+
+  #chat .chat-tabs {
+	display: flex;
+	justify-content: flex-end; /* Align to the right */
+  }
+
+  #chat .chat-tab {
+	width: 300px;
+	background-color: #413d3d;
+	border: none;
+	border-radius: 5px;
+	color: rgb(255, 196, 0);
+	cursor: pointer;
+	font-size: 1em;
+	padding: 5px 10px;
+	margin-left: 10px;
+	box-shadow: 3px 3px 5px rgba(255, 0, 128, 0.5); /* Pink shadow */
+  }
+
+  #chat .chat-tab:hover {
+	background-color: rgb(255, 193, 7);
+	color: grey;
+  }
+
+  #chat .chat-tab.active {
+	background-color: #ea00ffc9;
+	color: rgb(252, 215, 5);
+}
+
+</style>
---
+++ frontend/src/components/chat/ChatUserList.vue
@@ -0,0 +1,81 @@
+<template>
+
+<div class="chat-sidebar">
+	<h2>Connected Users</h2>
+	<ul class="chat-users">
+	  <!-- Connected users will be added here dynamically using JavaScript -->
+	  user1...
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+
+	</ul>
+	<h2>Connected Friends</h2>
+	<ul class="chat-users">
+	  <!-- Connected users will be added here dynamically using JavaScript -->
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	  <h5>test</h5>
+	</ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+	#chat .chat-sidebar {
+		width: 300px;
+		height: 407px;
+		border-right: 1px solid rgb(255, 204, 0);
+		padding: 10px;
+		overflow-y: auto;
+	}
+	#chat .chat-sidebar h2 {
+		margin: 0;
+		font-size: 1.1em;
+		color: rgb(255, 217, 0);
+		font-weight: normal;
+	}
+	// #chat .chat-sidebar .chat-users {
+	// 	list-style: none;
+	// 	margin: 0;
+	// 	padding: 0;
+	// }
+	// #chat .chat-sidebar .chat-user {
+	// 	display: flex;
+	// 	align-items: center;
+	// 	margin-bottom: 10px;
+	// }
+	// #chat .chat-sidebar .chat-user .chat-user-avatar {
+	// 	border-radius: 50%;
+	// 	width: 50px;
+	// 	height: 50px;
+	// 	margin-right: 10px;
+	// }
+	// #chat .chat-sidebar .chat-user .chat-user-username {
+	// 	font-weight: bold;
+	// }
+</style>
