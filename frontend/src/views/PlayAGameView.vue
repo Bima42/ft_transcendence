8be29bd96939f6369440d46871b9467a5c66d1d6
Tanguy Rossel <trossel@student.42lausanne.ch>
2022-12-21 15:07:06 +0100

    WIP: Play a game view

--- frontend/src/views/PlayAGameView.vue
+++ frontend/src/views/PlayAGameView.vue
@@ -1,9 +1,32 @@
 <template>
+<div id="menuList">
+<h1 class="mainTitle">Play a game</h1>
+<CustomButton>Random match</CustomButton>
+<CustomButton>Lobby</CustomButton>
+<CustomButton>Create Match</CustomButton>
+</div>
 </template>
 
 <script setup lang="ts">
+import CustomButton from '@/components/CustomButton.vue';
 </script>
 
 <style scoped lang="scss">
+
+.mainTitle {
+	font-size: 24px;
+	margin-bottom: 20px;
+
+}
+
+#menuList {
+  grid-area: $main;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+}
 
 </style>
