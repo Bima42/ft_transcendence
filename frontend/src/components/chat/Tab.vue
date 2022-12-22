37248a953059f695f312bbeb2e4e98a1e81955d4
santonie <sandrine.antonietti@gmail.com>
2022-12-22 17:03:53 +0100

    Trying to implements tabs in the chat

---
+++ frontend/src/components/chat/Tab.vue
@@ -0,0 +1,24 @@
+<template>
+	<div v-show="this.isActive">
+	  <slot></slot>
+	</div>
+</template>
+
+
+<script lang="ts">
+	export default {
+	  props: {
+		title: {
+		  type: String,
+		  default: 'Tab'
+		}
+	  },
+	  data () {
+		return {
+		  isActive: true
+		}
+	  }
+	}
+  </script>
+
+
