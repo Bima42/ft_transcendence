1a94dab77c0d0332220ad84874d092bf41fcfafb
santonie <sandrine.antonietti@gmail.com>
2022-12-30 19:14:06 +0100

    Modal "create a new channel" added to the chat

--- frontend/src/views/CommunityView.vue
+++ frontend/src/views/CommunityView.vue
@@ -1,10 +1,13 @@
 <template>
   <ChatMain></ChatMain>
+  <Modal></Modal>
 
 </template>
 
 <script setup lang="ts">
   import ChatMain from '@/components/chat/ChatMain.vue';
+  import Modal from '@/components/modal/Modal.vue';
+
 </script>
 
 <style scoped lang="scss">
