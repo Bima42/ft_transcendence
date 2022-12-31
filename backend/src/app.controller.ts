9031c86e23495fa3c64cb1e5474e8acea91e9b8c
Bima42 <tanpau42@gmail.com>
2022-12-31 14:47:20 +0100

    Removing some app files useless

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
@@ -7,7 +6,6 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
     private authService: AuthService,
   ) {}
 
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -26,6 +25,5 @@ import { DatabaseModule } from './modules/database.module';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
