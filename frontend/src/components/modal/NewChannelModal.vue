1a1812197affcdb5c1f40c970a8e8cc8eef2efee
Yvanoff <petruzzi.yvan@gmail.com>
2023-01-09 17:11:33 +0100

    fix: modified the newChannel modal structure. Need to be restyled

--- frontend/src/components/modal/NewChannelModal.vue
+++ frontend/src/components/modal/NewChannelModal.vue
@@ -1,33 +1,19 @@
 <template>
-  <div>
-    <transition name="modal">
-      <div v-if="isOpen">
-        <div class="overlay" @click.self="isOpen = false;">
-          <div class="modal">
-              <h1 class="title">Create a new channel</h1>
-              <p class="description">Here you can create a new channel, please fill the following info :</p>
-                <div class="chat-input-container">
-                  <div><input type="text" placeholder="Chose the name of the channel" class="chat-input"></div>
-                  <div><input type="text" placeholder="Set Password" class="chat-input"></div>
-                  <div><input type="text" placeholder="Confirm Password" class="chat-input"></div>
-                  <div><input type="text" placeholder="Invite users to the channel" class="chat-input">
-                </div>
-                <div class="radio-buttons">
-                  <label><input type="radio" name="type" value="private">Private</label>
-                  <label><input type="radio" name="type" value="public">Public</label>
-                </div>
-                <div class="button-container">
-                  <CustomButton>Create</CustomButton>
-                  <CustomButton>Quit</CustomButton>
-                </div>
-                </div>
-          </div>
-        </div>
-      </div>
-    </transition>
-    <CustomButton @click="isOpen = !isOpen;">
-      {{ isOpen ? "Create a new channel" : "Create a new channel" }}
-    </CustomButton>
+  <h1>Create a new channel</h1>
+  <p>Here you can create a new channel, please fill the following info :</p>
+  <div class="chat-input-container">
+    <input type="text" placeholder="Chose the name of the channel" class="chat-input">
+    <input type="text" placeholder="Set Password" class="chat-input">
+    <input type="text" placeholder="Confirm Password" class="chat-input">
+    <input type="text" placeholder="Invite users to the channel" class="chat-input">
+    <div class="radio-buttons">
+      <label><input type="radio" name="type" value="private">Private</label>
+      <label><input type="radio" name="type" value="public">Public</label>
+    </div>
+    <div class="button-container">
+      <CustomButton>Create</CustomButton>
+      <CustomButton @click="quitButton">Quit</CustomButton>
+    </div>
   </div>
 </template>
 
@@ -37,77 +23,48 @@ import CustomButton from '@/components/CustomButton.vue'
 
 const modalStore = useModalStore()
 const data = modalStore.data.data
-</script>
 
-<script lang = "ts">
-  export default {
-    data: function() {
-      return {
-        isOpen: false
-      };
-    }
-  };
+function quitButton(e: Event) {
+  modalStore.resetState()
+}
 </script>
 
 <style lang="scss" scoped>
+.radio-buttons {
+  color: rgb(255, 179, 0);
+  margin: 10px;
+  padding: 10px;
+}
 
-  .button-container button {
-    margin: 10px;
-  }
+h1 {
+  margin-bottom: 20px;
+  font-size: 2em;
+  font-weight: bold;
+  color: rgb(255, 179, 0);
+  text-align: center;
+}
 
-  .radio-buttons {
-    color: rgb(255, 179, 0);
-    margin: 10px;
-    padding: 10px;
-  }
+p {
+  font-style: italic;
+  color: rgb(128, 126, 126);
+}
 
-  .title{
-    margin-bottom: 20px;
-    font-size: 2em;
-    font-weight: bold;
-    color: rgb(255, 179, 0);
-    text-align: center;
-  }
+.chat-input-container {
+  display: flex;
+  flex-direction: column; /*aligns items vertically*/
+  align-items: center;
+  padding: 10px;
+}
 
-  .description {
-    font-style: italic;
-    color: rgb(128, 126, 126);
-  }
-
-    .chat-input-container {
-      display: flex;
-      flex-direction: column; /*aligns items vertically*/
-      align-items: center;
-      padding: 10px;
-    }
-    .chat-input {
-      color: rgb(255, 200, 0);
-      font-style: italic;
-      height: 30px;
-      width: 450px;
-      border: 1px solid rgb(255, 204, 0);
-      background-color: rgb(49, 49, 47);
-      border-radius: 5px;
-      padding: 0 10px; /*inside*/
-      margin-top: 20px;
-    }
-
-  .modal {
-    padding: 20px;
-    background-color: rgb(56, 53, 53);
-    border-radius: 10px;
-    box-shadow: 0 20px 50px rgba(211, 2, 214, 0.644);
-  }
-
-  .overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    background: #000000e8;
-  }
+.chat-input {
+  color: rgb(255, 200, 0);
+  font-style: italic;
+  height: 30px;
+  width: 450px;
+  border: 1px solid rgb(255, 204, 0);
+  background-color: rgb(49, 49, 47);
+  border-radius: 5px;
+  padding: 0 10px; /*inside*/
+  margin-top: 20px;
+}
 </style>
