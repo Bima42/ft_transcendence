716cfc6dcc437ec5a9efbd9fbcda292730f5e4d4
lbelet <laurentbelet@gmail.com>
2023-01-10 13:49:15 +0100

    WIPImplementedGameCanvas

--- frontend/src/game/pong.vue
+++
@@ -1,2 +0,0 @@
-blablablabla
-asfafasd
--- frontend/src/views/PlayAGameView.vue
+++ frontend/src/views/PlayAGameView.vue
@@ -1,9 +1,59 @@
 <template>
+    <section class="test">
+        <div>
+            <canvas id="game" width="800" height="600"></canvas>            
+            <div class="controls">
+                <button @click="startGame">Start</button>
+                <button @click="pauseGame">Pause</button>
+            </div>
+            <div class="score">
+                Player 1: {{ score1 }} Player 2: {{ score2 }}
+            </div>
+        </div>
+    </section>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+    export default {
+    data() {
+        return {
+        score1: 0,
+        score2: 0,
+        }
+    },
+    mounted() {
+        this.startGame();
+    },
+    methods: {
+        startGame() {
+            game.start();
+        },
+        pauseGame() {
+            game.pause();
+        }
+    }
+}
+
 </script>
 
 <style scoped lang="scss">
+    .test {
+        grid-area: $main;
+    }
+    canvas {
+    background-color: rgb(211, 59, 160);
+    display: block;
+    margin: auto;
+  }
 
+  .controls {
+    text-align: center;
+    margin-top: 10px;
+  }
+
+  .score {
+    text-align: center;
+    font-size: 24px;
+    color: white;
+  }
 </style>
