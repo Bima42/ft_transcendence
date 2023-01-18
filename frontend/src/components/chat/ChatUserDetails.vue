18177da295e95f19a6936c80d79b0615f38a2813
lbelet <laurentbelet@gmail.com>
2023-01-18 14:45:51 +0100

    WIP_Styled_ToolTip

--- frontend/src/components/chat/ChatUserDetails.vue
+++ frontend/src/components/chat/ChatUserDetails.vue
@@ -2,14 +2,25 @@
   <p @click="$emit('setActive')">
     {{ user.name }}
   <div class="user-details-panel" v-show="isActive">
+    <UserAvatar type="small"/>
     Actif
-    <button @click.stop="$emit('setInactive')">close</button>
+    <button @click.stop="$emit('setInactive')">close</button> <button v-on:click="addFriend">{{ buttonName }} </button> <button>Send private msg</button> <button @click="$emit('setActive')">...</button>
   </div>
   </p>
 </template>
 
 <script setup lang="ts">
-import {defineProps} from 'vue'
+
+var buttonName = "Add friend"
+
+import {defineProps} from 'vue';
+import UserAvatar from "@/components/UserAvatar.vue";
+
+  function addFriend () {
+    return {
+      buttonName: "remove"
+    }
+  }
 
 defineEmits(['setActive', 'setInactive'])
 
@@ -25,7 +36,7 @@ p {
   text-align: left;
 
   .user-details-panel {
-    width: 120px;
+    width: 400px;
     padding: 30px;
     border-radius: 10px;
     border: 5px solid yellow;
