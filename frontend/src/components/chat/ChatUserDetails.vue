6a10955eec6f7c08d00b6d33e9b8aea22154e769
santonie <sandrine.antonietti@gmail.com>
2023-01-22 20:32:57 +0100

    WIP tooltip

--- frontend/src/components/chat/ChatUserDetails.vue
+++ frontend/src/components/chat/ChatUserDetails.vue
@@ -4,30 +4,70 @@
   <div class="user-details-panel" v-show="isActive">
     <UserAvatar type="small"/>
     Actif
-    <button @click.stop="$emit('setInactive')">close</button> <button v-on:click="addFriend">{{ buttonName }} </button> <button>Send private msg</button> <button @click="$emit('setActive')">...</button>
+    <button @click.stop="$emit('setInactive')">close</button>
+    <!-- <button v-on:click="addFriend">{{ buttonName }} </button> <button>Send private msg</button> <button @click="$emit('setActive')">...</button> -->
+
+    <div class="box">
+	    <a class="button" href="#popup1">Add friend</a> <a class="button" href="#popup2">Send private msg</a> <a class="button" href="#popup3">...</a>
+    </div>
+
   </div>
   </p>
+
+  <div id="popup1" class="overlay">
+    <div class="popup">
+      <h2>"Username" Correctly added! :) </h2>
+      <a class="close" href="#">&times;</a>
+      <div class="content">
+        ...
+      </div>
+    </div>
+  </div>
+
+  <div id="popup2" class="overlay">
+    <div class="popup">
+      <h2>Send private messge to "Username"</h2>
+      <a class="close" href="#">&times;</a>
+      <div class="content">
+        <div class="chat-input-container">
+          <input type="text" placeholder="Type a message...">
+          <Button>Send</Button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="popup3" class="overlay">
+    <div class="popup">
+      <Button>Report user</Button>
+      <Button>Block user</Button>
+      <Button>Autre?</Button>
+      <a class="close" href="#">&times;</a>
+    </div>
+  </div>
+
 </template>
 
 <script setup lang="ts">
 
-var buttonName = "Add friend"
+  // var buttonName = "Add friend"
 
-import {defineProps} from 'vue';
-import UserAvatar from "@/components/UserAvatar.vue";
+  import {defineProps} from 'vue';
+  import UserAvatar from "@/components/UserAvatar.vue";
 
-  function addFriend () {
-    return {
-      buttonName: "remove"
-    }
-  }
+    // function addFriend () {
+    //   return {
+    //     buttonName: "remove"
+    //   }
+    // }
 
-defineEmits(['setActive', 'setInactive'])
+  defineEmits(['setActive', 'setInactive'])
 
-const props = defineProps<{
-  user: object,
-  isActive: boolean,
-}>()
+  const props = defineProps<{
+    user: object,
+    isActive: boolean,
+  }>()
+
 </script>
 
 <style scoped lang="scss">
@@ -46,4 +86,52 @@ p {
     z-index: 10;
   }
 }
+
+
+.button {
+  font-size: 1em;
+  background: #e2e2e2;
+  color: rgb(41, 39, 39);
+  border: 2px solid #2b2b2b;
+  border-radius: 20px/50px;
+  text-decoration: none;
+  cursor: pointer;
+}
+.button:hover {
+  background: #d8b806;
+}
+
+.overlay {
+  position: fixed;
+  width: 1000px;
+  opacity: 0;
+}
+.overlay:target {
+  opacity: 1;
+}
+
+.popup {
+  margin: 70px auto;
+  padding: 20px;
+  background: #fff;
+  border-radius: 5px;
+  width: 30%;
+  position: relative;
+}
+
+.popup h2 {
+  color: #333;
+  font-family: Tahoma, Arial, sans-serif;
+}
+.popup .close {
+  position: absolute;
+  top: 20px;
+  right: 30px;
+  transition: all 200ms;
+  font-size: 30px;
+  font-weight: bold;
+  text-decoration: none;
+  color: #333;
+}
+
 </style>
