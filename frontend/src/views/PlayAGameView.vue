18d83005f466b708523810269854492f10c05787
lbelet <laurentbelet@gmail.com>
2023-02-23 12:20:52 +0100

    Pong ok (?)

--- frontend/src/views/PlayAGameView.vue
+++ frontend/src/views/PlayAGameView.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="test">
+    <section class="gamePong">
         <div>
             <canvas id="game" width="900" height="600"></canvas>            
         </div>
@@ -15,12 +15,12 @@ export default {
     // declaration des variables
     var canvas = document.getElementById("game");
     var ctx = canvas.getContext("2d");
+
     var x = canvas.width/2;
     var y = canvas.height/2;
-    var dx = 5;
-    var dy = 0;
-    var dirx = 1;
-    var diry = 0;
+    var speedX = 5;
+    var speedY = 0;
+    var dirX = 1;
     var paddleHeight = 100;
     var paddleWidth = 20;
     var paddleLeftY = canvas.height/2;
@@ -28,6 +28,7 @@ export default {
     var reset = 0;
     var scoreRight = 0;
     var scoreLeft = 0;
+    var maxScore = 3;
 
     // dessin et mouvement de la balle
     function drawBall() {
@@ -36,69 +37,63 @@ export default {
         ctx.fillStyle = "black";
         ctx.fill();
         ctx.closePath();
-
-        if (x <= 850 && dirx == 1)
+    // mouvements axe X et Y
+        if (x >= 50 && x <= canvas.width -50)
         {
-            if (x == 850 && !(y >= (paddleRightY - 50) && y <= (paddleRightY + 50)))
+            // Rebond paddleRight
+            if (x == 850)
             {
-                scoreLeft++;
-                reset = 1;
-                x = canvas.width/2;
-            } 
-            x += dx;
-        }
-        else if (x >= 50)
-        {
-            if (x == 50 && !(y >= (paddleLeftY - 50) && y <= (paddleLeftY + 50)))
+                if (y > paddleRightY - 50 && y < paddleRightY + 50){
+                    speedX *= -1;
+                    if (y > paddleRightY - 50 && y < paddleRightY)
+                        speedY = -3;
+                    if (y < paddleRightY + 50 && y > paddleRightY)
+                        speedY = 3;
+                }
+                else{
+                    scoreLeft += 1;
+                    reset = 1;
+                }
+            }           
+            if (x == 50) // Rebond paddleLeft
             {
-                scoreRight++;
-                reset = 1;
-                x = canvas.width/2;
+                if (y > paddleLeftY - 50 && y < paddleLeftY + 50){
+                    speedX *= -1;
+                    if (y > paddleLeftY - 50 && y < paddleLeftY)
+                        speedY = -3;
+                    if (y < paddleLeftY + 50 && y > paddleLeftY)
+                        speedY = 3;
+                }
+                else{
+                    scoreRight += 1;
+                    reset = 1;
+                }
             }
-            dirx = -1;
-            x -= dx;
-        }
-        else
-            dirx = 1;
-
-        // if ((y > (canvas.height / canvas.height)) && diry == 0)
-        // {
-        //     y += dy;
-        // }
-        // else if (y < 599)
-        // {
-        //     diry = 1;
-        //     y -= dy;
-        // }
-        // else
-        //     diry = 0;
-    }
-    
+            x += speedX;
+            if (y <= 1|| y >= 599) // Rebond haut et bas
+            {
+                speedY *= -1;
+            }
+            y += speedY;
+        }    
+    }   
     // ligne de separation
     function drawMiddleLine() {
         ctx.fillRect(canvas.width/2 -2 ,0,4,600);
-    }
-    
+    }    
     // les paddles
     function drawPaddleLeft(){
         ctx.fillRect(20,paddleLeftY - 55 , paddleWidth, paddleHeight);
     }
-
     function drawpaddleRight(){
         ctx.fillRect(canvas.width - 40 ,paddleRightY - 55 , paddleWidth, paddleHeight);
     }
-
     // scores
-    function drawScoreRight(){
+    function drawScores(){
         ctx.font = '70px serif';
         ctx.fillText(scoreRight, 500, 80);
-    }
-    
-    function drawScoreLeft(){
-        ctx.font = '70px serif';
         ctx.fillText(scoreLeft, 350, 80);
     }
-
     // fin de partie
     function gameOver(){
         ctx.font = '100px serif';
@@ -106,25 +101,23 @@ export default {
     }
 
     function winner(){
-        if (scoreLeft == 3)
+        if (scoreLeft == maxScore)
         {
             ctx.font = '70px serif';
             ctx.fillText('Player left wins', 180, 500)
         }
-        else if (scoreRight == 3)
+        else if (scoreRight == maxScore)
         {
             ctx.font = '70px serif';
             ctx.fillText('Player right wins', 180, 500)
         }
     }
-
     // fonction principale
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         if(reset == 0)
         {
-            drawScoreRight();
-            drawScoreLeft();
+            drawScores();
             drawBall();
             drawMiddleLine();
             drawPaddleLeft();
@@ -132,19 +125,23 @@ export default {
         }
         else if (reset == 1)
         {
-            if (scoreLeft < 3 && scoreRight < 3){
+            if (scoreLeft < maxScore && scoreRight < maxScore){
                 paddleLeftY = canvas.height/2;
                 paddleRightY = canvas.height/2;
-                dirx = (-1*dirx);
+                dirX *= -1;
+                y = canvas.height/2;
+                x = canvas.width/2;
+                speedY = 0;
+                speedX = 5 * dirX;
                 reset = 0;
+
             }
             else
                 gameOver();
                 winner();
         }
     }
-
-    // ecoute des evenements clavier pour paddleRight
+    // ecoute des evenements clavier pour paddleRight (up/down)
     document.addEventListener("keydown", function(event) 
     {
         if (event.keyCode == 38 && paddleRightY != 50) // Touche "haut"
@@ -156,6 +153,7 @@ export default {
             paddleRightY += 10;
         }
     });
+    // ecoute des evenements clavier pour paddleLeft (w/s)
     document.addEventListener("keydown", function(event) 
     {
         if (event.keyCode == 87 && paddleLeftY != 50) // Touche "haut"
@@ -172,27 +170,15 @@ export default {
     setInterval(draw, 20);
   }
 };
-
 </script>
 
 <style scoped lang="scss">
-    .test {
+    .gamePong {
         grid-area: $main;
     }
     canvas {
     background-color: whitesmoke;
     display: block;
     margin: auto;
-  }
-
-  .controls {
-    text-align: center;
-    margin-top: 10px;
-  }
-
-  .score {
-    text-align: center;
-    font-size: 24px;
-    color: white;
   }
 </style>
