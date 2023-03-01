567817a9e9260406a68a94e3b5f558a6574e8590
YvanoffP <petruzzi.yvan@gmail.com>
2023-03-01 17:02:06 +0100

    WIP

---
+++ frontend/src/components/table/TheTablev3.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="table_wrapper">
+    <table>
+      <thead>
+        <tr>
+          <th>Opponent</th>
+          <th>Score</th>
+          <th>Population</th>
+          <th>Live</th>
+          <th>Game</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, index) in tableData" :key="index">
+          <td>{{ row.opponent }}</td>
+          <td>{{ row.score }}</td>
+          <td>{{ row.population }}</td>
+          <td>{{ row.live }}</td>
+          <td>{{ row.game }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  data: object,
+}>()
+
+const tableData = props.data;
+
+</script>
+
+<style scoped lang="scss">
+.table_wrapper {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  grid-area: $main;
+}
+
+</style>
--- frontend/src/views/LiveGameView.vue
+++ frontend/src/views/LiveGameView.vue
@@ -1,9 +1,20 @@
 <template>
-  <TheTable></TheTable>
+<!--  <TheTable></TheTable>-->
+  <TheTablev3 :data="tableData"></TheTablev3>
 </template>
 
 <script setup lang="ts">
-import TheTable from "@/components/table/TheTable.vue";
+import { ref } from 'vue'
+
+import TheTable from "@/components/table/TheTable.vue"
+import TheTablev3 from "@/components/table/TheTablev3.vue"
+
+const tableData = ref([
+  { opponent: 'Team A', score: 10, population: 100000, live: true, game: 'Game 1' },
+  { opponent: 'Team B', score: 5, population: 50000, live: false, game: 'Game 2' },
+  { opponent: 'Team C', score: 8, population: 80000, live: true, game: 'Game 3' },
+  { opponent: 'Team D', score: 3, population: 30000, live: false, game: 'Game 4' }
+])
 </script>
 
 <style scoped lang="scss">
