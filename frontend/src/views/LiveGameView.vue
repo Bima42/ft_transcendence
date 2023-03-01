e84a325d20f82bb4137db56e0aefb6fa1b6d1e87
Yvanoff <petruzzi.yvan@gmail.com>
2023-03-01 18:00:00 +0100

    wip

--- frontend/src/views/LiveGameView.vue
+++ frontend/src/views/LiveGameView.vue
@@ -10,10 +10,11 @@ import TheTable from "@/components/table/TheTable.vue"
 import TheTablev3 from "@/components/table/TheTablev3.vue"
 
 const tableData = ref([
-  { opponent: 'Team A', score: 10, population: 100000, live: true, game: 'Game 1' },
-  { opponent: 'Team B', score: 5, population: 50000, live: false, game: 'Game 2' },
-  { opponent: 'Team C', score: 8, population: 80000, live: true, game: 'Game 3' },
-  { opponent: 'Team D', score: 3, population: 30000, live: false, game: 'Game 4' }
+  { opponent: 'Team A', score: '10 - 1', population: 100000, live: true, game: 'Game 1' },
+  { opponent: 'Team B', score: '5 - 43', population: 50000, live: false, game: 'Game 2' },
+  { opponent: 'Team C', score: '8 - 1', population: 80000, live: true, game: 'Game 3' },
+  { opponent: 'Team D', score: '3 - 2', population: 30000, live: false, game: 'Game 4' },
+  { opponent: 'Team E', score: '4 - 3', population: 20, live: true, game: 'Game 23'}
 ])
 
 const tableHeader = ref([
