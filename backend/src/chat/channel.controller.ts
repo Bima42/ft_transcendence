361d577912ee351a988da135b0f91336b899d255
Tanguy Rossel <tanguy.rossel@tyrossel.ch>
2023-03-06 07:04:55 +0100

    feat(chat): add kick ban endpoints

--- backend/src/chat/channel.controller.ts
+++ backend/src/chat/channel.controller.ts
@@ -41,21 +41,32 @@ export class ChannelController {
 	  return this.channelService.postMessage(id, data);
   }
 
- //  @Put('rooms/:id/add/:user')
- //  AddUserToChat(@Param('id', new ParseIntPipe()) chatId: number,
-	// 			@Param('user', new ParseIntPipe()) userId: number) {
-	// 	return this.channelService.addUserToChat(chatId, userName);
-	// }
-	//
- //  @Put('rooms/:chat/kick/:user')
- //  KickUserFromChat(@Param('chat', new ParseIntPipe()) chatId: number,
-	// 			@Param('user', new ParseIntPipe()) userId: number) {
-	// 	return this.channelService.KickUserFromChat(chatId, userId)
-	// }
+  @Put('rooms/:id/mute/:user')
+  MuteUserToChat(@Param('id', new ParseIntPipe()) chatId: number,
+				@Param('user', new ParseIntPipe()) userId: number) {
+		return this.channelService.UpsertUserChatRole(chatId, userId, UserChatRole.MEMBER, 10);
+	}
 
-  @Put('rooms/:chat/ban/:user')
+  @Put('rooms/:id/add/:user')
+  AddUserToChat(@Param('id', new ParseIntPipe()) chatId: number,
+				@Param('user', new ParseIntPipe()) userId: number) {
+		return this.channelService.UpsertUserChatRole(chatId, userId, UserChatRole.MEMBER, 0);
+	}
+
+  @Put('rooms/:chat/kick/:user')
   KickUserFromChat(@Param('chat', new ParseIntPipe()) chatId: number,
 				@Param('user', new ParseIntPipe()) userId: number) {
 		return this.channelService.UpsertUserChatRole(chatId, userId, UserChatRole.BANNED, 0)
+	}
+
+  @Put('rooms/:chat/ban/:user')
+  BanUserFromChat(@Param('chat', new ParseIntPipe()) chatId: number,
+				@Param('user', new ParseIntPipe()) userId: number) {
+		return this.channelService.UpsertUserChatRole(chatId, userId, UserChatRole.BANNED, 0)
+	}
+
+	@Get('users/online')
+	GetOnlineUsers() {
+		return "me"
 	}
 }
