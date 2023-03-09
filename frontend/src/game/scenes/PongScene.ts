3cb16af779f92fe790f79114ee2187fefb43ad1d
Tanguy Rossel <tanguy.rossel@tyrossel.ch>
2023-03-09 10:35:14 +0100

    Switch to Typescript
    Remove unused assets

--- frontend/src/game/scenes/PongScene.ts
+++ frontend/src/game/scenes/PongScene.ts
@@ -1,6 +1,39 @@
-import { Scene } from 'phaser'
+import 'phaser'
+// import type GameObjects from 'phaser'
+//
+//
+class Ball extends Phaser.Physics.Arcade.Image {
 
-export default class PongScene extends Scene {
+	maxSpeed: number = 700;
+
+	constructor(scene: Phaser.Scene, x: number, y: number) {
+		super(scene, x, y, 'assets', 'ball1')
+		scene.add.existing(this);
+		scene.physics.add.existing(this);
+		this.setCollideWorldBounds(true).setBounce(1);
+	}
+
+}
+
+class Paddle extends Phaser.Physics.Arcade.Image {
+
+	constructor(scene: Phaser.Scene, x: number, y: number, nPlayer: number) {
+		super(scene, x, y, 'assets', 'paddle' + nPlayer)
+		scene.add.existing(this);
+		scene.physics.add.existing(this);
+		this.setImmovable();
+        this.angle = 90;
+		this.setSize(this.height, this.width, true);
+
+	}
+
+}
+
+export default class PongScene extends Phaser.Scene {
+
+    private ball: Ball;
+    private paddle1 : Paddle;
+    private paddle2 : Paddle;
 
     constructor() {
         super({ key: 'PongScene' })
@@ -11,28 +44,18 @@ export default class PongScene extends Scene {
 
     }
 
-    create()
+    create() : void
     {
         //  Enable world bounds, but disable the sides (left, right, up, down)
         this.physics.world.setBoundsCollision(false, false, true, true);
 
-        this.ball = this.physics.add.image(400, 500, 'assets', 'ball1').setCollideWorldBounds(true).setBounce(1);
+		this.ball = new Ball(this, 400, 300);
         this.ball.setData('onPaddle', true);
 
-        this.paddle1 = this.physics.add.image(20, 300, 'assets', 'paddle1').setImmovable();
-        this.paddle1.angle = 90;
-        let tmp = this.paddle1.width;
-        this.paddle1.width = this.paddle1.height;
-        this.paddle1.height = tmp;
-
-        this.paddle2 = this.physics.add.image(780, 300, 'assets', 'paddle2').setImmovable();
-        this.paddle2.setRotation(+ Math.PI / 2);
-
-        console.log("Paddle1 " + this.paddle1.width);
-        console.log("Paddle1 " + this.paddle1.height);
+        this.paddle1 = new Paddle(this, 20, 300, 1);
+        this.paddle2 = new Paddle(this, 780, 300, 2);
 
         //  Our colliders
-        // this.physics.add.collider(this.ball, this.bricks, this.hitBrick, null, this);
         this.physics.add.collider(this.ball, this.paddle1, this.hitPaddle, null, this);
         this.physics.add.collider(this.ball, this.paddle2, this.hitPaddle, null, this);
 
@@ -40,7 +63,7 @@ export default class PongScene extends Scene {
         this.input.on('pointermove', function (pointer) {
 
             //  Keep the paddle within the game
-            this.paddle1.y = Phaser.Math.Clamp(pointer.y, 52, 748);
+            this.paddle1.y = Phaser.Math.Clamp(pointer.y, 0, 800);
 
             if (this.ball.getData('onPaddle'))
             {
@@ -53,7 +76,7 @@ export default class PongScene extends Scene {
 
             if (this.ball.getData('onPaddle'))
             {
-                this.ball.setVelocity(-300, 0);
+                this.ball.setVelocity(-700, 0);
                 this.ball.setData('onPaddle', false);
             }
 
@@ -61,7 +84,7 @@ export default class PongScene extends Scene {
 
     }
 
-    resetBall()
+    private resetBall()
     {
         this.ball.setVelocity(0);
         this.ball.setPosition(400, 300);
@@ -75,18 +98,14 @@ export default class PongScene extends Scene {
 
     hitPaddle(ball, paddle)
     {
-        var minAngle = 20;
-        console.log("diff = " + ball.y);
-        var percentage = Phaser.Math.Clamp((ball.y - paddle.y) / 20, 0, 1)
-        console.log("percentage: " + percentage);
+        const minAngle = 30;
+        const percentage = Phaser.Math.Clamp((ball.y - paddle.y + paddle.width / 2) / paddle.width, 0, 1)
         var newAngle = 180 + minAngle + (180 - 2 * minAngle) * percentage;
         if (ball.x < 300)
             newAngle *= -1;
-        console.log("new angle: " + newAngle);
         // convert to radian
         newAngle *= Math.PI / 180;
-        ball.setVelocity(300 * Math.sin(newAngle), 300 * Math.cos(newAngle));
-        console.log("new velocity: " + JSON.stringify(ball.body.velocity));
+        ball.setVelocity(ball.maxSpeed * Math.sin(newAngle), ball.maxSpeed * Math.cos(newAngle));
 
         this.sound.play('thud', { volume: 0.75 })
     }
