6f6b1d1338779941a4f21b24eec72e7efd972b7a
Tanguy Rossel <trossel@student.42lausanne.ch>
2023-03-09 19:14:08 +0100

    Add a Join Queue Vue
    * A box with the settings for a new game (currently only type: classic
      or custom)
    * The view with the settings and the "join queue" button

---
+++ frontend/src/components/game/GameSettings.vue
@@ -0,0 +1,52 @@
+<template>
+	<div>
+	<CustomButton id="classic_btn" class="enabled_button" @click="classic_click">Classic 1972 game</CustomButton>
+	<CustomButton id="custom_btn" class="disabled_button" @click="custom_click">Hardcore game</CustomButton>
+	</div>
+</template>
+
+<script setup lang="ts">
+
+import CustomButton from '@/components/CustomButton.vue';
+
+  const classic_click = () => {
+	document.querySelector("#custom_btn").className = "disabled_button";
+	document.querySelector("#classic_btn").className = "enabled_button";
+    console.log("Classic !");
+  }
+
+  const custom_click = () => {
+	document.querySelector("#custom_btn").className = "enabled_button";
+	document.querySelector("#classic_btn").className = "disabled_button";
+    console.log("Custom !");
+  }
+</script>
+
+<style scoped lang="scss">
+button {
+  border-radius: 16px;
+  background: $yellow;
+  padding: 5px 12px;
+  border: none;
+
+  &:hover {
+    cursor: pointer;
+    filter: hue-rotate(180deg);
+  }
+
+  &.fat {
+    font-weight: 900;
+  }
+
+}
+  .enabled_button {
+	  background-color: $yellow;
+
+  }
+
+  .disabled_button {
+	  background-color: $lighter-gray;
+	  color: white;
+
+  }
+</style>
---
+++ frontend/src/views/JoinQueue.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="wrapper">
+	<GameSettings></GameSettings>
+	<CustomButton id="join_btn">Join queue</CustomButton>
+	</div>
+</template>
+
+<script setup lang="ts">
+
+import CustomButton from '@/components/CustomButton.vue'
+import GameSettings from '@/components/game/GameSettings.vue';
+
+
+</script>
+
+<style scoped lang="scss">
+	div {
+		padding: 26px;
+
+	}
+#wrapper {
+  display: flex;
+  justify-content: center;
+}
+
+#join_btn {
+	font-size: 20px;
+}
+
+</style>
