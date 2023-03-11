b4dbdde28a5f7c48e69c03151c1538e1e6c0b0b6
Yvanoff <petruzzi.yvan@gmail.com>
2023-03-11 12:23:59 +0100

    fix(review): small modifications and componentify

--- frontend/src/components/game/GameSettings.vue
+++ frontend/src/components/game/GameSettings.vue
@@ -1,76 +1,76 @@
 <template>
-  <h1> Please select a version of the game :</h1>
+    <h1> Please select a version of the game :</h1>
 
-	<div id="wrapper">
-  	<CustomButton id="classic_btn" class="enabled_button" @click="classic_click">Classic 1972 Pong</CustomButton>
-  	<CustomButton id="custom_btn" class="disabled_button" @click="custom_click">Hardcore Pong</CustomButton>
-	</div>
+    <div id="wrapper">
+        <CustomButton id="classic_btn" class="enabled_button" @click="classic_click">Classic 1972 Pong</CustomButton>
+        <CustomButton id="custom_btn" class="disabled_button" @click="custom_click">Hardcore Pong</CustomButton>
+    </div>
 
-  <h2> When you're ready to start playing, simply join the queue and wait for an opponent to be matched with. </h2>
+    <h2> When you're ready to start playing, simply join the queue and wait for an opponent to be matched with. </h2>
 </template>
 
 <script setup lang="ts">
 
 import CustomButton from '@/components/CustomButton.vue';
 
-  const classic_click = () => {
-	document.querySelector("#custom_btn").className = "disabled_button";
-	document.querySelector("#classic_btn").className = "enabled_button";
-    console.log("Classic !");
-  }
+const classic_click = () => {
+    document.querySelector('#custom_btn').className = 'disabled_button';
+    document.querySelector('#classic_btn').className = 'enabled_button';
+}
 
-  const custom_click = () => {
-	document.querySelector("#custom_btn").className = "enabled_button";
-	document.querySelector("#classic_btn").className = "disabled_button";
-    console.log("Custom !");
-  }
+const custom_click = () => {
+    document.querySelector('#custom_btn').className = 'enabled_button';
+    document.querySelector('#classic_btn').className = 'disabled_button';
+}
 </script>
 
 <style scoped lang="scss">
 
 #wrapper {
-  display: flex;
-  flex-direction: row;
-	justify-content: center;
-	grid-area: $main;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    grid-area: $main;
 }
+
 h1 {
-  text-align: center;
-  margin: 10px;
-  color: $yellow;
+    text-align: center;
+    margin: 10px;
+    color: $yellow;
 }
 
 h2 {
-  text-align: center;
-  margin: 10px 0 30px;
-  color: $yellow;
+    text-align: center;
+    margin: 10px 0 30px;
+    color: $yellow;
 }
 
 button {
-  border-radius: 16px;
-  background: $yellow;
-  padding: 5px 12px;
-  border: none;
-  margin : 10px;
+    border-radius: 16px;
+    background: $yellow;
+    padding: 5px 12px;
+    border: none;
+    margin: 10px;
 
-  &:hover {
-    cursor: pointer;
-    filter: hue-rotate(180deg);
-  }
+    &:hover {
+        cursor: pointer;
+        filter: hue-rotate(180deg);
+    }
 
-  &.fat {
-    font-weight: 900;
-  }
+    &.fat {
+        font-weight: 900;
+    }
 
 }
-  .enabled_button {
-	  background-color: $yellow;
 
-  }
+.enabled_button {
+    background-color: $yellow;
 
-  .disabled_button {
-	  background-color: $lighter-gray;
-	  color: white;
+}
 
-  }
+.disabled_button {
+    background-color: $lighter-gray;
+    color: white;
+
+}
 </style>
--- frontend/src/views/JoinQueue.vue
+++ frontend/src/views/JoinQueue.vue
@@ -1,86 +1,41 @@
 <template>
 
-<div id="wrapper">
+    <div id="wrapper">
 
-    <GameSettings></GameSettings>
-	<CustomButton id="join_btn" @click="joinQueue">
-	{{ abortQueue ? 'Abort' : 'Join queue' }}
-	</CustomButton>
-	<p v-if="joinText" class="join-text">{{ joinText }}</p>
-	<div class="box" v-if="showBox">
-		<ball></ball>
-	</div>
-</div>
+        <GameSettings></GameSettings>
+        <CustomButton id="join_btn" @click="joinQueue">
+            {{ isLoading ? 'Abort' : 'Join queue' }}
+        </CustomButton>
+        <LoadingGame v-if="isLoading"></LoadingGame>
+    </div>
 
 </template>
 
 <script setup lang="ts">
+import {ref} from 'vue';
+import CustomButton from '@/components/CustomButton.vue'
+import GameSettings from '@/components/game/GameSettings.vue'
+import LoadingGame from '@/components/game/LoadingGame.vue';
 
-import { ref } from 'vue';
-import CustomButton from '@/components/CustomButton.vue';
-import GameSettings from '@/components/game/GameSettings.vue';
-
-const joinText = ref('');
-const showBox = ref(false);
-const abortQueue = ref(false);
+const isLoading = ref(false)
 
 function joinQueue() {
-	if (!abortQueue.value) {
-		joinText.value = 'You have successfully joined the queue. Please wait for a few seconds while we find you a match.';
-		showBox.value = true;
-		abortQueue.value = true;
-	} else {
-		joinText.value = '';
-		showBox.value = false;
-		abortQueue.value = false;
-	}
+    isLoading.value = !isLoading.value
 }
 
 </script>
 
 <style scoped lang="scss">
-
-  .join-text {
-    color: $yellow;
-	text-align: center;
-  	margin: 10px 0 30px;
-  }
-
-  #wrapper {
+#wrapper {
     display: flex;
     flex-direction: column;
-	justify-content: center;
-	width: 50%;
-	grid-area: $main;
-  }
+    justify-content: center;
+    width: 50%;
+    grid-area: $main;
+}
 
-  #join_btn {
+#join_btn {
     font-size: 20px;
     margin-bottom: 20px;
-  }
-
-  .box {
-	display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 20px;
-    height: 30px;
-    position: relative;
-    border-left: 3px solid $yellow;
-    border-right: 3px solid $yellow;
-  }
-
-  .box ball {
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
-    background-color: $yellow;
-    position: absolute;
-    -webkit-animation: moveX 2.00s linear 0s infinite alternate;
-  }
-
-  @-webkit-keyframes moveX {
-    from { left: 0; } to { left: 98%; }
-  }
-
+}
 </style>
