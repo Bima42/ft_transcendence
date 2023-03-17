2af52eb3eb506576c4ea9747c65691e777a7f4e4
Tanguy Rossel <trossel@student.42lausanne.ch>
2023-03-17 16:46:02 +0100

    Chat: add background text when there is no messages in the chat

--- frontend/src/components/chat/ChatWindow.vue
+++ frontend/src/components/chat/ChatWindow.vue
@@ -1,12 +1,13 @@
 <template>
-	  <div class="chat-window-wrapper">
-		<MessageDisplay v-for="element in messages" :message="element"></MessageDisplay>
-	  </div>
+    <div class="chat-window-wrapper">
+        <h1 id="background_text">No messages :(</h1>
+        <MessageDisplay v-for="element in messages" :message="element"></MessageDisplay>
+    </div>
 </template>
 
 <script setup lang="ts">
-import {defineProps, ref, onMounted} from 'vue'
-import {get} from "../../../utils"
+import { defineProps, ref, onMounted } from 'vue'
+import { get } from "../../../utils"
 import MessageDisplay from "@/components/chat/MessageDisplay.vue";
 import type IChatMessage from '@/interfaces/chat/IChatMessage';
 import { useChatStore } from '@/stores/chat';
@@ -19,11 +20,18 @@ onMounted(() => {
     chatStore.$subscribe(async (mutation, state) => {
         const url = 'chat/rooms/' + chatStore.currentChat.id + "/messages";
 
+        messages.value = [];
         const tmp_messages = await get(url, 'Failed to get messages')
             .then((res) => res.json())
             .catch((err) => (console.log("Error: " + err)));
 
         messages.value = tmp_messages;
+        console.log("count = " + messages.value.length);
+        if (messages.value.length)
+            document.getElementById("background_text").style.display = "none";
+        else
+            document.getElementById("background_text").style.display = "block";
+
     });
 });
 
@@ -31,15 +39,22 @@ onMounted(() => {
 
 
 <style scoped lang="scss">
-.chat-window-wrapper {
-  flex-grow: 1;
-  width: 100%;
-  height: 80%;
-  position: relative;
+#background_text {
+    width: 100%;
+    height: 80%;
+    text-align: center;
 
-  display: flex;
-  flex-direction: column-reverse;
-  overflow: auto;
-  gap: 15px;
+}
+
+.chat-window-wrapper {
+    flex-grow: 1;
+    width: 100%;
+    height: 80%;
+    position: relative;
+
+    display: flex;
+    flex-direction: column-reverse;
+    overflow: auto;
+    gap: 15px;
 }
 </style>
