f2dfd0b35212dd19dd693a1d4d5b085f976ca3ae
Tanguy Rossel <trossel@student.42lausanne.ch>
2023-03-17 17:46:45 +0100

    chat: add socket to chatStore

--- frontend/src/stores/chat.ts
+++ frontend/src/stores/chat.ts
@@ -2,15 +2,17 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia';
 import type IChatMessage from '@/interfaces/chat/IChatMessage'
 import type IChat from '@/interfaces/chat/IChat'
+import { io } from "socket.io-client"
 
 export const useChatStore = defineStore('chat', () => {
-  let currentChatMessages = ref<IChatMessage[]>([]);
+    const socket = ref(io("ws://localhost:3080/chat"));
+    let currentChatMessages = ref<IChatMessage[]>([]);
+    let currentChat = ref<IChat>(null);
+    let chatsList = ref<IChat[]>([]);
 
-  let currentChat = ref<IChat>(null);
+    function addMessage(msg: IChatMessage): void {
+        this.currentChatMessages.push(msg);
+    }
 
-  function addMessage(msg: IChatMessage) : void {
-    this.currentChatMessages.push(msg);
-  }
-
-  return { currentChat, currentChatMessages, addMessage }
+    return { socket, currentChat, currentChatMessages, chatsList, addMessage }
 })
