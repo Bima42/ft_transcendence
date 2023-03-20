be322aa09b843abb8afedbf0fcedefee5a170259
Tanguy Rossel <tanguy.rossel@tyrossel.ch>
2023-03-20 10:21:11 +0100

    Rework env variables

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,6 +8,9 @@ done
 # Install dependencies
 yarn install
 
+# craft the var from all others:
+export DATABASE_URL="postgresql://${POSTGRES_USER:-db}:${POSTGRES_PASSWORD:-mypassword}@${POSTGRES_HOST:-db}:${POSTGRES_PORT:-5432}/${POSTGRES_DB:-postgres}?schema=public"
+
 # First start, create the database and run migrations
 npx prisma migrate dev --name init
 npx prisma migrate
