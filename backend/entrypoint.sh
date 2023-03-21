5881bf3713b6f7c24fc0a0d5df224defe35e82fc
tyrossel <trossel@student.42lausanne.ch>
2023-03-21 18:28:31 +0100

    Docker/consolidate (#70)
    * Rework env variables
    
    * feat(docker): file permission and rootless
    
    * fix(env): remove env files from git repo
    
    * feat(docker): file permission and rootless for frontend
    
    * fix(env): postgres env file
    
    * fix(postgres): dont expose the database on the host
    
    * feat(docker): backend now test connection to database before starting
    
    * feat(docker): put whole directories in image builds
    
    * revert(make): Restore node_modules and dist deletion on `make prune`
    
    * feat(env): use env vars for Oauth
    
    ---------
    
    Co-authored-by: Tanguy Rossel <tanguy.rossel@tyrossel.ch>

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,23 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Wait for the app to be mounted
 while [ ! -d /server ]; do
     sleep 1
 done
 
+# Wait for the database to be started
+while ! pg_isready -q --dbname="${POSTGRES_DB}" \
+                      --host="${POSTGRES_HOST}" \
+                      --port="${POSTGRES_PORT}"; do
+    echo "Waiting on database connection..."
+    sleep 1
+done
+
 # Install dependencies
 yarn install
+
+# craft the var from all others:
+export DATABASE_URL="postgresql://${POSTGRES_USER:-db}:${POSTGRES_PASSWORD:-mypassword}@${POSTGRES_HOST:-db}:${POSTGRES_PORT:-5432}/${POSTGRES_DB:-postgres}?schema=public"
 
 # First start, create the database and run migrations
 npx prisma migrate dev --name init
--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -6,7 +6,9 @@ export const useAuthStore = defineStore( 'auth', () => {
 	let user = localStorage.getItem('localUser') ? JSON.parse(localStorage.getItem('localUser')!) as IUser : null
 
 	const redirect = function () {
-		const redirect = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-175010164675e1a964dee80d5ce4a1ee18e22ba3fb6b0f7773673c3ff68c71d9&redirect_uri=https%3A%2F%2Flocalhost%3A4443%2Fapi%2Fauth%2F42%2Fcallback&response_type=code'
+    let redirect = 'https://api.intra.42.fr/oauth/authorize?client_id='
+    redirect += import.meta.env.VITE_FORTYTWO_API_UID + '&redirect_uri=';
+    redirect += encodeURIComponent(import.meta.env.VITE_FORTYTWO_API_CALLBACK) + '&response_type=code';
 		window.open(redirect, '_self')
 	}
 	const login = function () {
