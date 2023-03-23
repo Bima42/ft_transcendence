dd9f8147fc9c7e9a0bf8231385cad6b4c6ef769f
Tanguy Rossel <tanguy.rossel@tyrossel.ch>
2023-03-23 13:39:55 +0100

    feat(backend): Validate NewChannelDto

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Use ValidationPipe to validate all inputs
+  app.useGlobalPipes(new ValidationPipe());
 
   // Use cookie parser middleware
   app.use(cookieParser());
