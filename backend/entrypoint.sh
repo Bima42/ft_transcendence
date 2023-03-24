5c09bca0041fc27d96df5921cba1ca83956d033c
tyrossel <trossel@student.42lausanne.ch>
2023-03-24 10:16:13 +0100

    Docker/prod (#72)
    * feat(docker): production environment
    
    * fix(docker): fix backend in production
    
    * feat(docker): re-add HTTP port
    
    * fix(docker): fix frontend in production
    
    * fix(docker): change production config to use correct app URL
    
    not tested yet, will do it Soon(tm)
    
    * fix(docker): remove nginx from frontend container
    
    ---------
    
    Co-authored-by: Tanguy Rossel <tanguy.rossel@tyrossel.ch>

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,25 +2,36 @@
 
 # Wait for the app to be mounted
 while [ ! -d /server ]; do
+    echo "waiting on volume to be mounted..."
     sleep 1
 done
+
+# craft the var from all others:
+export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT:-5432}/${POSTGRES_DB}?schema=public"
 
 # Wait for the database to be started
 while ! pg_isready -q --dbname="${POSTGRES_DB}" \
                       --host="${POSTGRES_HOST}" \
                       --port="${POSTGRES_PORT}"; do
-    echo "Waiting on database connection..."
+    echo "Waiting on database connection... postgresql://${POSTGRES_USER}:<***>@${POSTGRES_HOST}:${POSTGRES_PORT:-5432}/${POSTGRES_DB}?schema=public"
     sleep 1
 done
 
-# Install dependencies
-yarn install
 
-# craft the var from all others:
-export DATABASE_URL="postgresql://${POSTGRES_USER:-db}:${POSTGRES_PASSWORD:-mypassword}@${POSTGRES_HOST:-db}:${POSTGRES_PORT:-5432}/${POSTGRES_DB:-postgres}?schema=public"
+if [ "${NODE_ENV:-production}" = "development" ];then
+    echo "Starting in development environment."
+    # First start, create the database and run migrations
+    if ! npx prisma migrate dev --name init; then
+      echo "Cannot initialize the database."
+      exit 1
+    fi
+else
+    echo "Starting in production environment."
+    if ! npx prisma migrate deploy ; then
+      echo "Cannot initialize the database."
+      exit 1
+    fi
+fi
 
-# First start, create the database and run migrations
-npx prisma migrate dev --name init
-npx prisma migrate
-
-yarn start:dev
+# Start the server with the specified CMD
+exec "$@"
