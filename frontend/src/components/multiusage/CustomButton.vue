7e3bdd686967956297352c179f88677dcd1ee013
Yvanoff <petruzzi.yvan@gmail.com>
2023-03-25 16:14:41 +0100

    modified add chat room modal and restyled x button

--- frontend/src/components/multiusage/CustomButton.vue
+++ frontend/src/components/multiusage/CustomButton.vue
@@ -1,34 +1,43 @@
 <template>
-  <button :class="props.styles">
-    <slot></slot>
-  </button>
+    <button :class="props.styles">
+        <slot></slot>
+    </button>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
-  styles?: String,
+    styles?: String,
 }>()
 
 </script>
 
 <style scoped lang="scss">
 button {
-  border-radius: 16px;
-  background: $yellow;
-  padding: 5px 12px;
-  border: none;
+    border-radius: 16px;
+    background: $yellow;
+    padding: 5px 12px;
+    border: none;
 
-  &:hover {
-    cursor: pointer;
-    filter: hue-rotate(180deg);
-  }
+    &:hover {
+        cursor: pointer;
+        filter: hue-rotate(180deg);
+    }
 
-  &.fat {
-    font-weight: 900;
-  }
+    &.fat {
+        font-weight: 900;
+    }
 
-  :disabled {
-    background: $light-gray;
-  }
+    &.cancel {
+        background: red;
+        color: white;
+
+        &:hover {
+            filter: hue-rotate(30deg);
+        }
+    }
+
+    :disabled {
+        background: $light-gray;
+    }
 }
 </style>
