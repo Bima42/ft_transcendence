ea830dc31e4de75db252448da32bb10dfc7a5f4c
Yvanoff <petruzzi.yvan@gmail.com>
2023-04-01 17:48:46 +0200

    added an interface

--- frontend/src/stores/chat.ts
+++ frontend/src/stores/chat.ts
@@ -5,7 +5,16 @@ import { io, Socket } from "socket.io-client"
 import { getCookie } from 'typescript-cookie';
 import { get } from '../../utils'
 
-export const useChatStore = defineStore('chat', () => {
+interface IChatStore {
+    socket: any;
+    currentChat: any;
+    chats: any;
+    sendMessage: (msg: any) => void;
+    setCurrentChat: (newChat: IChat) => Promise<IChat | null>;
+    refreshChatList: () => Promise<IChat[]>;
+}
+
+export const useChatStore = defineStore('chat', (): IChatStore => {
   const socket = ref<Socket>(io(`wss://${import.meta.env.VITE_APP_URL}/chat`, {
     auth: { token: getCookie("access_token") },
     path: "/api/socket.io/",
@@ -13,7 +22,7 @@ export const useChatStore = defineStore('chat', () => {
   const currentChat = ref<IChat | null>(null);
   const chats = ref<IChat[]>([]);
 
-  const sendMessage = function(this: any, msg: any): void {
+  const sendMessage = function(this: IChatStore, msg: any): void {
     this.socket.emit("msg", msg, (answer: any) => {
       // Handle from here if the server answer something (i.e error)
       // for example 'banned', 'muted', etc.
@@ -21,7 +30,7 @@ export const useChatStore = defineStore('chat', () => {
     });
   }
 
-  const setCurrentChat = async function(this: any, newChat: IChat): Promise<IChat | null> {
+  const setCurrentChat = async function(this: IChatStore, newChat: IChat): Promise<IChat | null> {
     if (!newChat)
       newChat = this.chats[0];
     const url = 'chat/rooms/' + newChat.id;
@@ -34,7 +43,7 @@ export const useChatStore = defineStore('chat', () => {
     return this.currentChat;
   }
 
-  const refreshChatList = async function (this: any): Promise<IChat[]> {
+  const refreshChatList = async function (this: IChatStore): Promise<IChat[]> {
     const url = 'chat/rooms/';
     await get(url, 'Cannot load channel list')
         .then((res) => res.json())
