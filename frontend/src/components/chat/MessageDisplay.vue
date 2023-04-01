b4f7b86e7ba67c8511cc3a1058a93cdd1dc776c3
Tanguy Rossel <tanguy.rossel@tyrossel.ch>
2023-04-01 12:20:28 +0200

    fix(chat): put the message on the correct side

--- frontend/src/components/chat/MessageDisplay.vue
+++ frontend/src/components/chat/MessageDisplay.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script setup lang="ts">
-import {defineProps, computed} from 'vue'
+import {defineProps, ref, computed, onUpdated} from 'vue'
 import UserAvatar from "@/components/multiusage/UserAvatar.vue";
 import { useUserStore } from "@/stores/user"
 import type IChatMessage from '@/interfaces/chat/IChatMessage';
@@ -18,10 +18,14 @@ const props = defineProps<{
   message: IChatMessage,
 }>()
 
-let text_position = props.message.userId as number;
+let text_position = ref(props.message.userId as number);
+
+onUpdated(() => {
+    text_position.value = props.message.userId as number;
+})
 
 const textPosition = computed(() => {
-  return text_position === authStore.user.id ? 'right' : 'left'
+  return text_position.value === authStore.user?.id ? 'right' : 'left'
 })
 </script>
 
