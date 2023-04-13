e0b9cfb0bc00bb80da3ef64be589199689c5a93b
Bima42 <tanpau42@gmail.com>
2023-04-13 17:26:34 +0200

    fix(stats): update route in stats controller

--- backend/src/users/stats/userstats.controller.ts
+++ backend/src/users/stats/userstats.controller.ts
@@ -4,35 +4,35 @@ import { ApiTags } from '@nestjs/swagger';
 import { PlayerStatsDto } from '../dto/user.dto';
 import { UserStatsService } from './userstats.service';
 
-@Controller('user/stats')
-@ApiTags('user/stats')
+@Controller('users/stats')
+@ApiTags('users/stats')
 @UseGuards(JwtAuthGuard)
 export class UserStatsController {
 	constructor(
 		private readonly userStatsService: UserStatsService
 	) {}
 
-	@Get('stats/games/:id')
+	@Get('games/:id')
 	async getGamesByUserId(@Param('id', ParseIntPipe) userId: number) {
 		return this.userStatsService.getPlayedGamesByUserId(userId);
 	}
 
-	@Get('stats/games/win/:id')
+	@Get('games/win/:id')
 	async getWonGamesByUserId(@Param('id', ParseIntPipe) userId: number) {
 		return this.userStatsService.getWonGamesByUserId(userId);
 	}
 
-	@Get('stats/games/winrate/:id')
+	@Get('games/winrate/:id')
 	async getWinRateByUserId(@Param('id', ParseIntPipe) userId: number) {
 		return this.userStatsService.getWinRateByUserId(userId);
 	}
 
-	@Get('stats/games/elo/:id')
+	@Get('games/elo/:id')
 	async getEloByUserId(@Param('id', ParseIntPipe) userId: number) {
 		return this.userStatsService.getEloByUserId(userId);
 	}
 
-	@Get('stats/:id')
+	@Get(':id')
 	async getStatsByUserId(@Param('id', ParseIntPipe) userId: number): Promise<PlayerStatsDto> {
 		return this.userStatsService.getStatsByUserId(userId);
 	}
