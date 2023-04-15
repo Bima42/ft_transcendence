170cc0b742e30eb228582a2820c6d9d3afcb6318
Yvanoff <petruzzi.yvan@gmail.com>
2023-04-15 14:33:31 +0200

    chore(chat): chat message comp is ok, now logical time

--- frontend/src/components/v2/chat/ChatElements.vue
+++ frontend/src/components/v2/chat/ChatElements.vue
@@ -1,6 +1,14 @@
 <template>
-    <div class="chat_element" v-for="x in 20">
-        <h3>Message {{ x }}</h3>
+    <div class="chat_element">
+        <ChatMessage
+            v-for="x in 20"
+            :key="x"
+            :fromOther="x % 2 === 0"
+        >
+            Message number kshjdbfjkh sdfbgkhjdsfbgkj sfdhbgikuj sbjksbg jkhsbg kjsdbfg kjsbdkj gbsdkjgb skhjgb kblkasdjflkajslfkjsdl
+            kfjlksd jflksdj flsjlf jlalfjlsdhfgjklsdhg jklshdgkjl sdjkg hsdkjlgh sjkdhg jksdhg kjsdhg kjsdhhkjgbsdjlhg i\shdgf ljksdg lhjk\sdkfg
+            hb\sldkhgkj;\sdfgi\uskdhg k;j\shdgjik h {{ x }}
+        </ChatMessage>
     </div>
 </template>
 
@@ -12,6 +20,7 @@
  * @param {number} chatId - This is the chat ID that is received from the parent
  */
 import { defineProps, ref } from 'vue'
+import ChatMessage from '@/components/v2/chat/ChatMessage.vue';
 
 const props = defineProps<{
     chatId: number
@@ -23,7 +32,11 @@ const chatId = ref(props.chatId)
 <style scoped lang="scss">
 .chat_element {
     display: flex;
+    width: 100%;
+    height: 100%;
     flex-direction: column-reverse;
+    overflow-y: scroll;
+    gap: 10px;
+    padding: 15px;
 }
-
 </style>
---
+++ frontend/src/components/v2/chat/ChatMessage.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="message_wrap" :class="[fromOther ? 'left' : 'right']">
+        <div class="sender" v-show="fromOther">
+            Sender
+        </div>
+        <div class="content">
+            <slot></slot>
+        </div>
+        <div class="time">
+            12:23
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, ref } from 'vue'
+
+const props = defineProps<{
+    fromOther: boolean
+}>()
+
+const fromOther = ref(props.fromOther)
+</script>
+
+<style scoped lang="scss">
+.message_wrap {
+    position: relative;
+    display: flex;
+    padding: 15px 10px;
+    flex-direction: column;
+    justify-content: center;
+    text-align: start;
+    max-width: 85%;
+    border-radius: 10px;
+    gap: 5px;
+
+    &.left {
+        align-self: flex-start;
+        background-color: $transparent-quaternary;
+    }
+    &.right {
+        align-self: flex-end;
+        background-color: $transparent-tertiary;
+    }
+
+    .sender {
+        font-weight: bold;
+        &:hover {
+            cursor: pointer;
+        }
+    }
+    .content {
+        font-size: 14px;
+    }
+    .time {
+        position: absolute;
+        font-size: 10px;
+        align-self: end;
+        line-height: 5px;
+        bottom: 5px;
+        right: 5px;
+    }
+}
+</style>
