d3e5c78b4c71d991c128921da34a5cf5ea8ac76d
Yvanoff <petruzzi.yvan@gmail.com>
2023-04-22 17:41:55 +0200

    feat(store): refacto send msg from store

--- frontend/src/components/chat/TypeBox.vue
+++ frontend/src/components/chat/TypeBox.vue
@@ -5,16 +5,17 @@
             @click="sendMessage"
             :style="'small'"
         >
-            <font-awesome-icon icon="fa-solid fa-paper-plane" /></ButtonCustom>
+            <font-awesome-icon icon="fa-solid fa-paper-plane"/>
+        </ButtonCustom>
         <ChatCommandHelper/>
     </div>
 </template>
 
 <script setup lang="ts">
-import { put } from '../../../utils';
-import { ref } from 'vue'
-import { useChatStore } from '@/stores/chat';
-import { useUserStore } from '@/stores/user';
+import {put} from '../../../utils';
+import {ref} from 'vue'
+import {useChatStore} from '@/stores/chat';
+import {useUserStore} from '@/stores/user';
 import ChatCommandHelper from '@/components/chat/chatcommands/ChatCommandHelper.vue';
 import type IChatAction from '@/interfaces/chat/IChatAction';
 import ButtonCustom from '@/components/v2/buttons/ButtonCustom.vue';
@@ -24,82 +25,72 @@ const chatStore = useChatStore();
 const userStore = useUserStore();
 
 async function executeCommand(cmd: string[]) {
-  if (! chatStore.currentChat )
-    return;
+    if (!chatStore.currentChat)
+        return;
 
-  const commandsList = [
-            // owner commands
-            'deleteChannel',  // Delete the current channel
-            'setPassword',    // Set the password to join a protected channel
+    const commandsList = [
+        // owner commands
+        'deleteChannel',  // Delete the current channel
+        'setPassword',    // Set the password to join a protected channel
 
-            // admin commands
-            'add',            // add a user to the channel
-            'kick',           // kick a user from the channel
-            'ban',            // ban a user from the channel
-            'mute',           // mute the user for a while
-            'promote',        // make a user channel administrator
-            'demote',         // make a user a simple user
+        // admin commands
+        'add',            // add a user to the channel
+        'kick',           // kick a user from the channel
+        'ban',            // ban a user from the channel
+        'mute',           // mute the user for a while
+        'promote',        // make a user channel administrator
+        'demote',         // make a user a simple user
 
-            // user commands
-            'leave',           // Leave the current channel
-            'addFriend',       // Add another user to its friends
-            'invite',          // Invite another user to play !
-            ];
+        // user commands
+        'leave',           // Leave the current channel
+        'addFriend',       // Add another user to its friends
+        'invite',          // Invite another user to play !
+    ];
 
-  const cmdName = cmd[0].slice(1);
-  let url = `chat/rooms/${chatStore.currentChat.id}/user`;
-  let action: IChatAction;
-  switch (cmdName) {
-    case 'kick':
-    case 'add':
-    case 'ban':
-    case 'promote':
-    case 'demote':
-    case 'mute':
-      action = {
-        chatId: chatStore.currentChat.id,
-        username: cmd[1],
-        muteDuration: (cmd.length >= 3 ? parseInt(cmd[2]) : null),
-        type: cmdName,
-      };
-      await put(url, `cannot ${cmdName} user`, action)
-      .catch(err => console.error(err))
-      break;
+    const cmdName = cmd[0].slice(1);
+    let url = `chat/rooms/${chatStore.currentChat.id}/user`;
+    let action: IChatAction;
+    switch (cmdName) {
+        case 'kick':
+        case 'add':
+        case 'ban':
+        case 'promote':
+        case 'demote':
+        case 'mute':
+            action = {
+                chatId: chatStore.currentChat.id,
+                username: cmd[1],
+                muteDuration: (cmd.length >= 3 ? parseInt(cmd[2]) : null),
+                type: cmdName,
+            };
+            await put(url, `cannot ${cmdName} user`, action)
+                .catch(err => console.error(err))
+            break;
 
-    case 'leave':
-      url = `chat/rooms/${chatStore.currentChat.id}/leave`;
-      await put(url, `cannot leave channel`, {})
-      .then(() => {
-        chatStore.setCurrentChat(null);
-      })
-      .catch(err => console.error(err))
-      break;
-    default:
-      console.error(`unknown chat command: ${cmdName}`);
-      break;
-  }
+        case 'leave':
+            url = `chat/rooms/${chatStore.currentChat.id}/leave`;
+            await put(url, `cannot leave channel`, {})
+                .then(() => {
+                    chatStore.setCurrentChat(null);
+                })
+                .catch(err => console.error(err))
+            break;
+        default:
+            console.error(`unknown chat command: ${cmdName}`);
+            break;
+    }
 }
 
 async function sendMessage() {
-  const msg = msgContent.value.trim();
-  if (!chatStore.currentChat || msg == "") return;
-
-  // Check for chat commands
-  if (msg.charAt(0) == '/') {
-    executeCommand(msg.split(' '));
-  }
-  else {
-
-    // Format: NewMessageDto from back
+    const msg = msgContent.value.trim();
+    if (!chatStore.currentChat || msg == '') return;
     const sentMsg = {
-      content: msgContent.value,
-      senderId: userStore.user?.id,
-      chatId: chatStore.currentChat.id,
+        content: msgContent.value,
+        senderId: userStore.user?.id,
+        chatId: chatStore.currentChat.id,
     };
-
     chatStore.sendMessage(sentMsg);
-  }
-  msgContent.value = "";
+    msgContent.value = '';
 }
 </script>
 
--- frontend/src/stores/chat.ts
+++ frontend/src/stores/chat.ts
@@ -4,100 +4,90 @@ import type IChat from '@/interfaces/chat/IChat'
 import { io, Socket } from 'socket.io-client'
 import { getCookie } from 'typescript-cookie'
 import { get } from '../../utils'
-import type { Ref } from 'vue'
+import type IChatStore from '@/interfaces/chat/IChatStore'
+import type ISendMessage from '@/interfaces/chat/ISendMessage'
 
-interface IChatStore {
-    socket: any;
-    currentChat: Ref<IChat | null>;
-    chats: Ref<IChat[]>;
-    isChatOpen: Ref<Boolean>;
-    sendMessage: (msg: any) => void;
-    setCurrentChat: (chatId: string) => Promise<boolean>;
-    retrievePublicChats: () => Promise<IChat[]>;
-    retrieveWhispers: () => Promise<IChat[]>;
-    getMessages: () => void;
-    resetState: () => void;
-}
 
 export const useChatStore = defineStore('chat', (): IChatStore => {
-  const socket = ref<Socket>(io(`wss://${import.meta.env.VITE_APP_URL}/chat`, {
-    auth: { token: getCookie("access_token") },
-    path: "/api/socket.io/",
-  }));
-  const currentChat = ref<IChat | null>(null);
-  const chats = ref<IChat[]>([]);
-  const isChatOpen = ref<boolean>(false);
+	const socket = ref<Socket>(io(`wss://${import.meta.env.VITE_APP_URL}/chat`, {
+		auth: {token: getCookie('access_token')},
+		path: '/api/socket.io/',
+	}));
+	const currentChat = ref<IChat | null>(null);
+	const chats = ref<IChat[]>([]);
+	const isChatOpen = ref<boolean>(false);
 
-  const sendMessage = function(this: IChatStore, msg: any): void {
-    this.socket.emit("msg", msg, (answer: any) => {
-      // Handle from here if the server answer something (i.e error)
-      // for example 'banned', 'muted', etc.
-      console.log("answer = " + JSON.stringify(answer));
-    });
-  }
+	const sendMessage = function (this: IChatStore, msg: ISendMessage): void {
+		if (!currentChat) return
+		this.socket.emit('msg', msg, (answer: any) => {
+			// Handle from here if the server answer something (i.e error)
+			// for example 'banned', 'muted', etc.
+			console.log('answer = ' + JSON.stringify(answer));
+		});
+	}
 
-  const setCurrentChat = async function(chatId: string): Promise<boolean> {
-      if (!chatId) {
-          return false
-      }
-      const url = 'chat/rooms/' + chatId;
-      await get(url, 'Cannot load channel')
-          .then((res) => res.json())
-          .then((newChannel) => {
-              currentChat.value = newChannel;
-          })
-          .catch((err) => console.log(err));
-      return true
-  }
+	const setCurrentChat = async function (chatId: string): Promise<boolean> {
+		if (!chatId) {
+			return false
+		}
+		const url = 'chat/rooms/' + chatId;
+		await get(url, 'Cannot load channel')
+			.then((res) => res.json())
+			.then((newChannel) => {
+				currentChat.value = newChannel;
+			})
+			.catch((err) => console.log(err));
+		return true
+	}
 
-    const getMessages = async function () {
-        if (!currentChat.value) return
-        isChatOpen.value = true;
-        const url = 'chat/rooms/' + currentChat.value.id + "/messages";
-        currentChat.value.messages = await get(url, 'Failed to get messages')
-            .then((res) => res.json())
-            .catch((err) => {
-                console.error(err)
-            });
-    }
-  const retrievePublicChats = async function (): Promise<IChat[]> {
-      let chats: IChat[] = [];
-      await get('chat/rooms', 'Cannot load public channels')
-          .then((res) => res.json())
-          .then((chatList: IChat[]) => {
-              chats = chatList
-          })
-          .catch((err) => console.log('ChatStore error: ', err));
-      return chats;
-  }
+	const getMessages = async function () {
+		if (!currentChat.value) return
+		isChatOpen.value = true;
+		const url = 'chat/rooms/' + currentChat.value.id + '/messages';
+		currentChat.value.messages = await get(url, 'Failed to get messages')
+			.then((res) => res.json())
+			.catch((err) => {
+				console.error(err)
+			});
+	}
+	const retrievePublicChats = async function (): Promise<IChat[]> {
+		let chats: IChat[] = [];
+		await get('chat/rooms', 'Cannot load public channels')
+			.then((res) => res.json())
+			.then((chatList: IChat[]) => {
+				chats = chatList
+			})
+			.catch((err) => console.log('ChatStore error: ', err));
+		return chats;
+	}
 
-  const retrieveWhispers = async function (): Promise<IChat[]> {
-      let chats: IChat[] = [];
-        await get('chat/rooms?whispers=true', 'Failed to retrieve whispers list')
-            .then((res) => res.json())
-            .then((chatList: IChat[]) => (
-                chats = chatList
-            ))
-            .catch((err) => (console.log('ChatStore error: ', err)));
-        return chats;
-  }
+	const retrieveWhispers = async function (): Promise<IChat[]> {
+		let chats: IChat[] = [];
+		await get('chat/rooms?whispers=true', 'Failed to retrieve whispers list')
+			.then((res) => res.json())
+			.then((chatList: IChat[]) => (
+				chats = chatList
+			))
+			.catch((err) => (console.log('ChatStore error: ', err)));
+		return chats;
+	}
 
-  const resetState = () => {
-    currentChat.value = null;
-    isChatOpen.value = false;
-    chats.value = [];
-  }
+	const resetState = () => {
+		currentChat.value = null;
+		isChatOpen.value = false;
+		chats.value = [];
+	}
 
-  return {
-      socket,
-      currentChat,
-      chats,
-      isChatOpen,
-      sendMessage,
-      setCurrentChat,
-      getMessages,
-      retrievePublicChats,
-      retrieveWhispers,
-      resetState
-  }
+	return {
+		socket,
+		currentChat,
+		chats,
+		isChatOpen,
+		sendMessage,
+		setCurrentChat,
+		getMessages,
+		retrievePublicChats,
+		retrieveWhispers,
+		resetState
+	}
 })
