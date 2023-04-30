17e662f1367450d48cfcbbadada1dc60069cf284
Yvanoff <44474562+YvanoffP@users.noreply.github.com>
2023-04-30 14:30:57 +0200

    Front/chat/userinteractions (#121)
    * first modal template
    
    * feat(userinformation): added userinformation modal, only front implemented

---
+++ frontend/src/components/chat/UserActions.vue
@@ -0,0 +1,92 @@
+<template>
+    <section class="user_actions">
+        <h2>{{ modalStore.data.user.username }}</h2>
+        <ButtonCustom :style="'big'" :click="toggleNewModal">
+            View profile
+        </ButtonCustom>
+        <section class="chat_actions_buttons">
+            <template v-if="userRole === role.admin">
+                <h4>Admin actions</h4>
+                <section class="button_wrap">
+                    <ButtonCustom :style="'small'">
+                        kick
+                    </ButtonCustom>
+                    <ButtonCustom :style="'small'">
+                        ban
+                    </ButtonCustom>
+                    <ButtonCustom :style="'small'">
+                        mute
+                    </ButtonCustom>
+                    <ButtonCustom :style="'small'">
+                        promote
+                    </ButtonCustom>
+                    <ButtonCustom :style="'small'">
+                        demote
+                    </ButtonCustom>
+                </section>
+            </template>
+            <template v-if="userRole <= role.user">
+                <section class="button_wrap">
+                    <ButtonCustom :style="'small'">
+                        unblock / block
+                    </ButtonCustom>
+                    <ButtonCustom :style="'small'">
+                        add / remove friend
+                    </ButtonCustom>
+                </section>
+            </template>
+        </section>
+    </section>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import ButtonCustom from '@/components/buttons/ButtonCustom.vue'
+import { useModalStore } from '@/stores/modal'
+import TheModal from '@/components/modal/TheModal.vue'
+import UserInformations from '@/components/modal/UserInformationsModal.vue'
+import type IUser from '@/interfaces/user/IUser';
+
+enum role { admin = 0, user = 1 }
+
+const modalStore = useModalStore()
+
+const props = defineProps<{}>()
+
+// WE NEED A FUNCTION TO CATCH THE ROLE OF A USER TO DISPLAY WHAT HE CAN AND CANNOT DO
+// onMounted(async () => {
+//     await userRole = chatStore.getRole()
+// })
+const userRole = role.user
+
+const toggleNewModal = () => {
+    modalStore.resetStateKeepData()
+    modalStore.loadAndDisplay(TheModal, UserInformations, {...modalStore.data})
+}
+</script>
+
+<style scoped lang="scss">
+.user_actions {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    justify-content: center;
+    align-items: center;
+
+    .chat_actions_buttons {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        gap: 10px;
+        flex-wrap: wrap;
+
+        .button_wrap {
+            display: flex;
+            justify-content: center;
+            flex-direction: row;
+            gap: 5px;
+            flex-wrap: wrap;
+        }
+    }
+}
+</style>
---
+++ frontend/src/components/modal/UserInformationsModal.vue
@@ -0,0 +1,98 @@
+<template>
+    <section class="user_information_modal">
+        <section class="user_data">
+            <UserAvatar :type="'small'" :url="modalStore.data.user.avatar"></UserAvatar>
+            <section class="data">
+                <h2>{{ modalStore.data.user.username }}</h2>
+                <section class="button_wrap">
+                    <ButtonCustom :style="'small'" :click="checkIfFriend">
+                        {{ isFriend ? 'Remove friend' : 'Add friend' }}
+                    </ButtonCustom>
+                    <ButtonCustom :style="'small'" :click="checkIfBlocked">
+                        {{ isBlocked ? 'Unblock' : 'Block' }}
+                    </ButtonCustom>
+                    <ButtonCustom :style="'small'">
+                        Invite to play a game
+                    </ButtonCustom>
+                </section>
+            </section>
+        </section>
+        <hr>
+        <section class="user_score">
+            <h4>Score</h4>
+            <h4>Some big numbers whew</h4>
+        </section>
+    </section>
+</template>
+
+<script setup lang="ts">
+import { defineProps, ref } from 'vue'
+import { useModalStore } from '@/stores/modal'
+import UserAvatar from '@/components/avatar/UserAvatar.vue';
+import ButtonCustom from '@/components/buttons/ButtonCustom.vue';
+
+const modalStore = useModalStore()
+
+const props = defineProps<{}>()
+const isBlocked = ref(false)
+const isFriend = ref(false)
+
+const checkIfBlocked = () => {
+    // check if user is blocked
+    // store it into isBlocked.data
+    isBlocked.value = !isBlocked.value
+}
+
+const checkIfFriend = () => {
+    // check if user is friend
+    // store it into isFriend.data
+    isFriend.value = !isFriend.value
+}
+</script>
+
+<style scoped lang="scss">
+.user_information_modal {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    justify-content: center;
+
+    hr {
+        border-color: $tertiary;
+        width: 80%;
+        align-self: center;
+    }
+
+    .user_data {
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+        justify-content: center;
+        align-items: center;
+
+        .data {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            justify-content: center;
+            align-items: flex-start;
+
+            .button_wrap {
+                display: flex;
+                flex-direction: row;
+                justify-content: flex-start;
+                gap: 5px;
+                flex-wrap: wrap;
+            }
+        }
+    }
+    .user_score {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        justify-content: center;
+        align-items: center;
+    }
+}
+
+</style>
