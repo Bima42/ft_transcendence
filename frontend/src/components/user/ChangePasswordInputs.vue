4a249f8ecc4a608ec35b1e233d339aa62eabdb9d
Yvanoff <petruzzi.yvan@gmail.com>
2023-04-30 18:28:50 +0200

    enhancement(usercred): removed change password option as it is 42 password. not app password

--- frontend/src/components/user/ChangePasswordInputs.vue
+++ frontend/src/components/user/ChangePasswordInputs.vue
@@ -1,5 +1,6 @@
 <template>
     <section class="password_change_wrapper">
+        <h2>Change password</h2>
         <label for="pass">Password (8 characters minimum):</label>
         <input type="password" id="pass" name="password"
                minlength="8" required>
@@ -15,5 +16,11 @@ const props = defineProps<{}>()
 </script>
 
 <style scoped lang="scss">
-
+.password_change_wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    justify-content: center;
+    padding: 10px 40px 10px 40px;
+}
 </style>
