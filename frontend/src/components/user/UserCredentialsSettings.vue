de52b1881751b6efcf81b5a1b6f9b3626f8bba33
Yvanoff <44474562+YvanoffP@users.noreply.github.com>
2023-04-30 22:07:14 +0200

    83/front/twofa (#132)
    * enh(twofa): now tickbox does not query on click and QR code is generable once
    
    * fix(2fa): can now be querried once

--- frontend/src/components/user/UserCredentialsSettings.vue
+++ frontend/src/components/user/UserCredentialsSettings.vue
@@ -4,13 +4,14 @@
         @click="openModal('TwoFA')"
         :style="'small'"
     >
-        Manage TwoFA
+        {{ userStore.user?.twoFA ? 'Deactivate TwoFA' : 'Activate TwoFA' }}
     </ButtonCustom>
 </template>
 
 <script setup lang="ts">
 import { defineProps } from 'vue'
 import { useModalStore } from '@/stores/modal'
+import { useUserStore } from '@/stores/user'
 import ButtonCustom from '@/components/buttons/ButtonCustom.vue'
 import UsernameInput from '@/components/user/UsernameInput.vue'
 import TwoFaInputs from '@/components/user/TwoFaInputs.vue'
@@ -18,10 +19,17 @@ import Modal from '@/components/modal/TheModal.vue'
 
 const props = defineProps<{}>()
 const modalStore = useModalStore()
+const userStore = useUserStore()
 
 const openModal = (modalName: string) => {
     if (modalName === 'TwoFA') {
-        modalStore.loadAndDisplay(Modal, TwoFaInputs, {})
+        if (userStore.user?.twoFA) {
+            userStore.updateTwoFaStatus(false)
+            return
+        }
+        else {
+            modalStore.loadAndDisplay(Modal, TwoFaInputs, {})
+        }
     }
 }
 </script>
