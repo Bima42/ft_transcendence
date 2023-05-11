5e2e33d3714d6acbc7d764f7a65ff11f94a22612
Tanguy Rossel <trossel@student.42lausanne.ch>
2023-05-11 08:46:12 +0200

    feat(chat): hide admin/owner options to other users

--- frontend/src/components/modal/channel/EditChatModal.vue
+++ frontend/src/components/modal/channel/EditChatModal.vue
@@ -2,11 +2,13 @@
     <div class="option_wrap">
         <h2>Select the actions you wish to perform</h2>
         <ButtonCustom :style="'big'" :click="() => handleClick('manageChannelUsers')">
-            <h1>manage channel users</h1>
+            <h1>Channel users</h1>
         </ButtonCustom>
+		<template v-if="userRole >= UserChatRoleEnum.Owner">
         <ButtonCustom :style="'big'" :click="() => handleClick('manageChannel')">
-            <h1>manage channel</h1>
+            <h1>Manage channel</h1>
         </ButtonCustom>
+		</template>
         <ButtonCustom :style="'big'" :click="() => handleClick('inviteFriend')">
             <h1>Invite friend</h1>
         </ButtonCustom>
@@ -20,13 +22,18 @@
 import ButtonCustom from '@/components/buttons/ButtonCustom.vue'
 import { useModalStore } from '@/stores/modal'
 import { useChatStore } from '@/stores/chat'
+import { useUserStore } from '@/stores/user'
 import TheModal from '@/components/modal/TheModal.vue'
 import manageChannelUsersModal from '@/components/modal/channel/ManageChannelUsersModal.vue'
 import manageChannelModal from '@/components/modal/channel/ManageChannelModal.vue'
 import chatInviteFriendModal from '@/components/modal/channel/ChatInviteFriendModal.vue'
+import { ref } from 'vue'
+import { UserChatRoleEnum } from '@/interfaces/user/IUserChat'
 
 const modalStore = useModalStore()
 const chatStore = useChatStore()
+const userStore = useUserStore()
+const userRole = ref(chatStore.getRoleFromUserId(userStore.user?.id || 0))
 
 const handleClick = (option: string) => {
     switch (option) {
