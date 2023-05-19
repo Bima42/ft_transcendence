2b4c5952c252ee9b15093c0f9daf0a4b2ee3fad3
Tanguy <87313922+Bima42@users.noreply.github.com>
2023-05-20 00:03:20 +0200

    feat(notifiication) implement notification system (#199)
    * feat(notification): notif component done and ready to use
    
    * feat(chat): get a notification on whisper
    
    * feat(notification): notif only on whisper
    
    * chore(seed): mattia diallo in the seed
    
    * lazy(quickndirty): refactored App.vue
    
    * fix(2fa): fix dumb catch
    
    ---------
    
    Co-authored-by: Tanguy Rossel <tanguy.rossel@tyrossel.ch>

---
+++ frontend/src/stores/notification.ts
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import type INotification from '@/interfaces/INotification';
+
+export const useNotificationStore = defineStore('notification', () => {
+	const show = ref(true);
+	const notifications = ref<INotification[]>([]);
+
+	const resetState = function () {
+		show.value = true;
+		notifications.value = [];
+	}
+
+	const addNotification = function (notification: INotification) {
+		notifications.value.push(notification);
+		setTimeout(() => {
+			removeNotification(notification)
+		}, notification.lifespan);
+	}
+
+	const removeNotification = function (notification: INotification) {
+		notifications.value.splice(notifications.value.indexOf(notification), 1);
+	}
+
+	return {
+		show,
+		notifications,
+		resetState,
+		addNotification,
+		removeNotification,
+	}
+});
