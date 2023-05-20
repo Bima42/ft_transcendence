efcb0398d7926148ff55a60d032a3e1d4866a983
Bima42 <tanpau42@gmail.com>
2023-05-20 13:02:29 +0200

    feat(notif): notif on friend connection

--- frontend/src/MainPage.vue
+++ frontend/src/MainPage.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script setup lang="ts">
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import CreditLink from '@/components/footers/CreditLink.vue'
 import ElSidebar from '@/components/template/ElSidebar.vue'
 import TheModal from '@/components/modal/TheModal.vue'
