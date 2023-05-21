f8dab32c1b6fb95f5653e215d549a0a6597025ca
YvanoffP <petruzzi.yvan@gmail.com>
2023-05-21 15:37:11 +0200

    fix(credits): removed dead end in credits

--- frontend/src/components/footers/GitHubCard.vue
+++ frontend/src/components/footers/GitHubCard.vue
@@ -1,45 +1,46 @@
 <template>
-  <section class="github-card">
-    <a :href="props.link" target="_blank">
-      <img style="width: 100px" src="@/assets/img/GitHub.svg"/>
-    </a>
-    <slot></slot>
-  </section>
+	<section class="github-card">
+		<a :href="props.link" target="_blank">
+			<img style="width: 100px" src="@/assets/img/GitHub.svg" alt="github"/>
+		</a>
+		<slot></slot>
+	</section>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
-  link: string,
+	link: string,
 }>()
 </script>
 
 <style lang="scss">
 .github-card {
-  min-width: 25%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: $small_gap;
+	min-width: 25%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: $small_gap;
 
-  h2 {
-    font-size: 18px;
-  }
+	h2 {
+		font-size: 18px;
+	}
 
-  .quote {
-    font-style: italic;
-    font-weight: bold;
-    font-size: 16px;
-  }
+	.quote {
+		font-style: italic;
+		font-weight: bold;
+		font-size: 16px;
+	}
 
-  img {
-    transform: scale(100%);
-    transition: all .2s ease-in-out;
-    &:hover {
-      transform: scale(95%);
-      transition: all .2s ease-in-out;
-    }
-  }
+	img {
+		transform: scale(100%);
+		transition: all .2s ease-in-out;
+
+		&:hover {
+			transform: scale(95%);
+			transition: all .2s ease-in-out;
+		}
+	}
 }
 
 </style>
--- frontend/src/views/CreditsView.vue
+++ frontend/src/views/CreditsView.vue
@@ -1,50 +1,74 @@
 <template>
-    <section class="credit-wrapper">
-        <GitHubCard :link="creator.githubLink" v-for="creator in creators">
-            <h2>{{ creator.name }}</h2>
-            <p class="quote">{{ "~ " + creator.title }}</p>
-        </GitHubCard>
-    </section>
+	<section class="credit_wrapper">
+		<section class="editors">
+			<GitHubCard :link="creator.githubLink" v-for="(creator, i) in creators" :key="i" :id="creator.name">
+				<h2>{{ creator.name }}</h2>
+				<p class="quote">{{ "~ " + creator.title }}</p>
+			</GitHubCard>
+		</section>
+		<section class="back_button">
+			<ButtonCustom :style="'big'" :click="() => $router.push('/')">
+				Back to home
+			</ButtonCustom>
+		</section>
+	</section>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import {ref} from 'vue'
 import GitHubCard from '@/components/footers/GitHubCard.vue'
+import ButtonCustom from '@/components/buttons/ButtonCustom.vue';
 
 const creators = ref(
-    [
-        {
-            name: 'Bima42',
-            title: 'BackMan',
-            githubLink: 'https://github.com/Bima42',
-            website: ''
-        },
-        {
-            name: 'YvanoffP',
-            title: 'FrontMan',
-            githubLink: 'https://github.com/YvanoffP',
-            website: ''
-        },
-        {
-            name: 'Tyrossel',
-            title: 'GeniusMan',
-            githubLink: 'https://github.com/Tyrossel',
-            website: ''
-        },
-    ]
+	[
+		{
+			name: 'Bima42',
+			title: 'BackMan',
+			githubLink: 'https://github.com/Bima42',
+			website: ''
+		},
+		{
+			name: 'YvanoffP',
+			title: 'FrontMan',
+			githubLink: 'https://github.com/YvanoffP',
+			website: ''
+		},
+		{
+			name: 'Tyrossel',
+			title: 'GeniusMan',
+			githubLink: 'https://github.com/Tyrossel',
+			website: ''
+		},
+	]
 )
 </script>
 
 <style scoped lang="scss">
-.credit-wrapper {
-    grid-area: $gigamain;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    gap: 50px;
+.credit_wrapper {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+
+	.editors {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+		gap: 50px;
+	}
+
+	.back_button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 20%;
+	}
 }
 </style>
