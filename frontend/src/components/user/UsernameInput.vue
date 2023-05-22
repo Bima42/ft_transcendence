bbe4f012b80f424281bdc9878ccf4c44c4f07332
Tanguy Rossel <trossel@student.42lausanne.ch>
2023-05-22 09:11:22 +0200

    chore: tab to spaces

--- frontend/src/components/user/UsernameInput.vue
+++ frontend/src/components/user/UsernameInput.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="userinput-wrapper">
-        <template v-if="editing">
-            <h3>{{ userValue }}</h3>
-            <ButtonCustom @click="startEditing" :style="'small'">
-                <font-awesome-icon icon="fa-pencil" />
-            </ButtonCustom>
-        </template>
-        <template v-else>
-            <input v-model="userValue" @keydown.enter="save" />
-            <font-awesome-icon @click="save" icon="fa-solid fa-check" />
-            <font-awesome-icon @click="cancel" icon="fa-solid fa-xmark" />
-        </template>
-    </div>
+	<div class="userinput-wrapper">
+		<template v-if="editing">
+			<h3>{{ userValue }}</h3>
+			<ButtonCustom @click="startEditing" :style="'small'">
+				<font-awesome-icon icon="fa-pencil" />
+			</ButtonCustom>
+		</template>
+		<template v-else>
+			<input v-model="userValue" @keydown.enter="save" />
+			<font-awesome-icon @click="save" icon="fa-solid fa-check" />
+			<font-awesome-icon @click="cancel" icon="fa-solid fa-xmark" />
+		</template>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -27,68 +27,70 @@ const editing = ref(true);
 
 const userValue = ref(userStore.user?.username)
 const startEditing = () => {
-    editing.value = !editing.value;
+	editing.value = !editing.value;
 };
 
 const save = () => {
-    editing.value = !editing.value;
-    if (!userValue.value)
-      return
+	editing.value = !editing.value;
+	if (!userValue.value)
+		return
 	const newValue = userValue.value.trim()
 	if (newValue.length < 2 || newValue.length > 20 || !newValue.match("^[a-zA-Z0-9_-]+$")) {
 		alertStore.setErrorAlert("username constraints: between 2 and 20 characters and contains only alphanumericals characters and/or -_")
 		return
 	}
-    const infos: IUserUpdate = { username: userValue.value }
-	userStore.updateInfos(infos).then((res) => {
+	const infos: IUserUpdate = { username: userValue.value }
+	userStore.updateInfos(infos).then(() => {
 		userValue.value = userStore.user?.username
 	})
 };
 
 const cancel = () => {
-    editing.value = !editing.value;
+	editing.value = !editing.value;
 };
 </script>
 <style scoped lang="scss">
 .userinput-wrapper {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap: $small_gap;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	gap: $small_gap;
 
-    input {
-        border: none;
-        border-bottom: 1px solid black;
-        background: transparent;
-        outline: none;
-        text-align: center;
-        color: white;
-        padding: 1px 6px;
-        width: auto;
-        max-width: 70%;
-    }
+	input {
+		border: none;
+		border-bottom: 1px solid black;
+		background: transparent;
+		outline: none;
+		text-align: center;
+		color: white;
+		padding: 1px 6px;
+		width: auto;
+		max-width: 70%;
+	}
 
-    .fa-pencil {
-        color: $quaternary;
-        &:hover {
-            cursor: pointer;
-            filter: hue-rotate(180deg);
-        }
-    }
+	.fa-pencil {
+		color: $quaternary;
 
-    .fa-xmark {
-        color: red;
-        &:hover {
-            cursor: pointer;
-        }
-    }
+		&:hover {
+			cursor: pointer;
+			filter: hue-rotate(180deg);
+		}
+	}
 
-    .fa-check {
-        color: green;
-        &:hover {
-            cursor: pointer;
-        }
-    }
-}
-</style>
+	.fa-xmark {
+		color: red;
+
+		&:hover {
+			cursor: pointer;
+		}
+	}
+
+	.fa-check {
+		color: green;
+
+		&:hover {
+			cursor: pointer;
+		}
+	}
+}</style>
